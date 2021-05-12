28e7d1b7d8df58a61a406240915f862c21c6e6ec
TrustedMercury <kevin.jt2007@gmail.com>
2021-05-11 20:39:28 -0400

    Added tags to server card, fixed tooltips

--- src/components/core/ServerCard.jsx
+++ src/components/core/ServerCard.jsx
@@ -8,20 +8,20 @@ function ServerCard(props) {
 
   return (
     <div className="flex flex-col items-center justify-between w-124 h-72 bg-dark-70 overflow-hidden rounded-xl">
-      <div className="flex flex-col items-center justify-between w-full h-23/30 p-8">
-        <div className="flex flex-row items-center justify-start w-full space-x-4">
+      <ReactTooltip
+        effect="solid"
+        className="server-status-tooltip"
+        backgroundColor="#000"
+        arrowColor="#000"
+      />
+      <div className="flex flex-col items-center justify-start w-full h-23/30 p-8 space-y-4">
+        <div className="flex flex-row items-center justify-start w-full space-x-4 select-none">
           <div className="relative flex flex-col items-center justify-center">
             <img
               src={props.favicon || "/images/default_favicon.png"}
               className="w-24 rounded-full"
               onError={(e) => (e.target.src = "/images/default_favicon.png")}
               draggable="false"
-            />
-            <ReactTooltip
-              effect="solid"
-              className="server-status-tooltip"
-              backgroundColor="#000"
-              arrowColor="#000"
             />
             <div
               className={`absolute w-8 h-8 -bottom-1 -right-1 ${
@@ -30,24 +30,38 @@ function ServerCard(props) {
               data-tip={props.online ? "Online" : "Offline"}
             />
           </div>
-          <div className="flex flex-col items-start justify-center">
-            <div className="flex flex-row items-center justify-start space-x-3">
-              <h1 className="font-proxima font-bold text-xl text-gray-300">
-                {props.name}
-              </h1>
-              <div
-                className="flex flex-row items-center justify-center rounded-full px-3 space-x-1 bg-dark-80 bg-opacity-[0.8]"
-                data-tip="Upvotes"
-              >
-                <i className="fas fa-angle-up text-olive-50" />
-                <h6 className="font-proxima font-bold text-gray-400 cursor-default">
-                  {props.monthly_votes || 0}
-                </h6>
+          <div className="flex flex-col items-start justify-center space-y-1">
+            <div className="flex flex-col items-start justify-center">
+              <div className="flex flex-row items-center justify-start space-x-3">
+                <h1 className="font-proxima font-bold text-xl text-gray-300">
+                  {props.name}
+                </h1>
+                <div
+                  className="flex flex-row items-center justify-center rounded-full px-3 space-x-1 bg-dark-80 bg-opacity-[0.8]"
+                  data-tip="Upvotes"
+                >
+                  <i className="fas fa-angle-up text-olive-50" />
+                  <h6 className="font-proxima font-bold text-gray-400 cursor-default">
+                    {props.monthly_votes || 0}
+                  </h6>
+                </div>
               </div>
+              <span className="font-proxima font-bold text-gray-400">
+                {props.players_online || 0} players
+              </span>
             </div>
-            <span className="font-proxima font-bold text-gray-400">
-              {props.players_online || 0} players
-            </span>
+            <div className="flex flex-row items-center justify-start space-x-2">
+              {props.tags.map((tag, index) => (
+                <div
+                  key={index}
+                  className="flex flex-row items-center justify-center px-2 py-1 bg-dark-80"
+                >
+                  <span className="font-semibold text-xs text-gray-400">
+                    {tag}
+                  </span>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
         <p className="font-medium text-sm text-gray-400">{props.description}</p>
