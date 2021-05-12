b9190463a58c5900c43ad6b9dbd91639bd412b00
TrustedMercury <kevin.jt2007@gmail.com>
2021-05-11 22:21:55 -0400

    Added google loading page

---
+++ src/pages/login/google.jsx
@@ -0,0 +1,9 @@
+function Google(props) {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full bg-dark-80">
+      <img src="/images/loading-1.gif" className="w-24" />
+    </div>
+  );
+}
+
+export default Google;
