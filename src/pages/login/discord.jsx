820206a674af398cf5c0a1d135d7e0dd851f5b69
TrustedMercury <kevin.jt2007@gmail.com>
2021-05-11 22:21:26 -0400

    Added loading gif and page

---
+++ src/pages/login/discord.jsx
@@ -0,0 +1,9 @@
+function Discord(props) {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full bg-dark-80">
+      <img src="/images/loading-1.gif" className="w-24" />
+    </div>
+  );
+}
+
+export default Discord;
