3624db8fa355c32798edd9e9dfe7a72f66cad9d2
TrustedMercury <kevin.jt2007@gmail.com>
2021-05-14 19:26:52 -0400

    Added tags data

---
+++ src/data/tags.js
@@ -0,0 +1,89 @@
+const categories = [
+  { id: 1, name: "versions", label: "Versions", tags: versions },
+  { id: 2, name: "minigames", label: "Minigames", tags: minigames },
+  { id: 3, name: "gamemodes", label: "Gamemodes", tags: gamemodes },
+  { id: 4, name: "features", label: "Features", tags: features },
+  { id: 5, name: "language", label: "Language", tags: languages },
+];
+
+const versions = [
+  { id: 0, name: "1.17", label: "1.17" },
+  { id: 1, name: "1.16.5", label: "1.16.5" },
+  { id: 2, name: "1.16.4", label: "1.16.4" },
+  { id: 3, name: "1.16.3", label: "1.16.3" },
+  { id: 4, name: "1.16.1", label: "1.16.1" },
+  { id: 5, name: "1.16", label: "1.16" },
+  { id: 6, name: "1.15.2", label: "1.15.2" },
+  { id: 7, name: "1.15.1", label: "1.15.1" },
+  { id: 8, name: "1.15", label: "1.15" },
+  { id: 9, name: "1.14.4", label: "1.14.4" },
+  { id: 10, name: "1.14", label: "1.14" },
+  { id: 11, name: "1.13.2", label: "1.13.2" },
+  { id: 12, name: "1.13", label: "1.13" },
+  { id: 13, name: "1.12.2", label: "1.12.2" },
+  { id: 14, name: "1.12", label: "1.12" },
+  { id: 15, name: "1.11.2", label: "1.11.2" },
+  { id: 16, name: "1.11", label: "1.11" },
+  { id: 17, name: "1.10.2", label: "1.10.2" },
+  { id: 18, name: "1.10", label: "1.10" },
+  { id: 19, name: "1.9.4", label: "1.9.4" },
+  { id: 20, name: "1.9", label: "1.9" },
+  { id: 21, name: "1.8.9", label: "1.8.9" },
+  { id: 22, name: "1.8", label: "1.8" },
+];
+
+const gamemodes = [
+  { id: 26, name: "pixelmon", label: "Pixelmon" },
+  { id: 28, name: "prison", label: "Prison" },
+  { id: 35, name: "skyblock", label: "Skyblock" },
+  { id: 36, name: "roleplay", label: "Roleplay" },
+  { id: 37, name: "creative", label: "Creative" },
+  { id: 38, name: "redstone", label: "Redstone" },
+  { id: 39, name: "anarchy", label: "Anarchy" },
+  { id: 41, name: "survival", label: "Survival" },
+  { id: 42, name: "adventure", label: "Adventure" },
+  { id: 43, name: "smp", label: "SMP" },
+  { id: 44, name: "ftb", label: "FTB" },
+  { id: 45, name: "tekkit", label: "Tekkit" },
+  { id: 46, name: "hardcore", label: "Hardcore" },
+  { id: 47, name: "towny", label: "Towny" },
+  { id: 48, name: "vanilla", label: "Vanilla" },
+  { id: 49, name: "modded", label: "Modded" },
+  { id: 66, name: "rpg", label: "RPG" },
+  { id: 67, name: "pokemon", label: "Pokemon" },
+];
+
+const minigames = [
+  { id: 70, name: "arena", label: "Arena" },
+  { id: 24, name: "parkour", label: "Parkour" },
+  { id: 31, name: "kitPvP", label: "KitPvP" },
+  { id: 32, name: "skywars", label: "Skywars" },
+  { id: 33, name: "bedwars", label: "Bedwars" },
+  { id: 34, name: "spleef", label: "Spleef" },
+  { id: 61, name: "minigames", label: "Minigames" },
+  { id: 62, name: "hunger_games", label: "Hunger Games" },
+  { id: 68, name: "zombies", label: "Zombies" },
+];
+
+const features = [
+  { id: 23, name: "puzzle", label: "Puzzle" },
+  { id: 25, name: "mcmmo", label: "MCMMO" },
+  { id: 27, name: "economy", label: "Economy" },
+  { id: 40, name: "plots", label: "Plots" },
+  { id: 29, name: "pvp", label: "PvP" },
+  { id: 30, name: "pve", label: "PvE" },
+  { id: 69, name: "anti_grief", label: "Anti-Grief" },
+  { id: 71, name: "fantasy", label: "Fantasy" },
+];
+
+const languages = [
+  { id: 72, name: "english", label: "English" },
+  { id: 57, name: "italiana", label: "Italiana" },
+  { id: 58, name: "española", label: "Española" },
+  { id: 59, name: "français", label: "Français" },
+  { id: 60, name: "polskie", label: "Polskie" },
+  { id: 73, name: "türk", label: "Türk" },
+  { id: 74, name: "pусский", label: "Pусский" },
+  { id: 75, name: "deutsche", label: "Deutsche" },
+  { id: 76, name: "中国人", label: "中国人" },
+];
