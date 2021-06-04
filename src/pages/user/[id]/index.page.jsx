f02747bf230524716bb7e1836c6822370fa4fb50
Kevin Thomas <kevin.jt2007@gmail.com>
2021-06-04 15:22:01 -0400

    Added servers to user page

--- src/pages/user/[id]/index.page.jsx
+++ src/pages/user/[id]/index.page.jsx
@@ -1,9 +1,14 @@
+import { Fragment } from "react";
 import { useQuery } from "react-query";
 import { useRouter } from "next/router";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 import getAuth from "../../../api/auth";
 import getUser from "../../../api/user/[id]";
 import StandardLayout from "../../../layouts/Standard";
+import ServerCard, {
+  ServerCardSkeleton,
+} from "../../../components/core/ServerCard";
 
 function User(props) {
   const router = useRouter();
@@ -15,33 +20,77 @@ function User(props) {
     return "sus";
   }
 
-  if (isLoading) {
-    return "loading";
-  }
-
   return (
     <StandardLayout user={props.user}>
-      <div className="flex flex-col items-center justify-start w-full h-full py-32 px-10 lg:px-20 2xl:px-56 bg-dark-80">
+      <div className="flex flex-col items-start justify-start w-full py-32 px-10 lg:px-20 2xl:px-56 space-y-16 bg-dark-80">
         <div className="flex flex-row items-center justify-start w-full space-x-6">
           <div className="flex flex-col items-center justify-center">
-            <img
-              src={
-                data.payload.minecraft_uuid
-                  ? `https://crafatar.com/avatars/${data.payload.minecraft_uuid}`
-                  : "/images/default_user.png"
-              }
-              className="w-32"
-            />
+            {isLoading ? (
+              <SkeletonTheme color="#222822" highlightColor="#2C322C">
+                <Skeleton width={128} height={128} />
+              </SkeletonTheme>
+            ) : (
+              <img
+                src={
+                  data.payload.minecraft_uuid
+                    ? `https://crafatar.com/avatars/${data.payload.minecraft_uuid}`
+                    : "/images/default_user.png"
+                }
+                className="w-32"
+              />
+            )}
           </div>
-          <div className="flex flex-col items-start justify-center">
-            <h1 className="font-bold text-4xl text-gray-300">
-              {data.payload.name}
-            </h1>
-            <p className="max-w-xs overflow-hidden text-xl text-gray-400">
-              {data.payload.description}
-            </p>
-          </div>
+          <SkeletonTheme color="#222822" highlightColor="#2C322C">
+            <div className="flex flex-col items-start justify-center">
+              {isLoading ? (
+                <Skeleton width={150} height={18} />
+              ) : (
+                <h1 className="font-bold text-4xl text-gray-300">
+                  {data.payload.name}
+                </h1>
+              )}
+              {isLoading ? (
+                <Fragment>
+                  <Skeleton width={400} />
+                  <Skeleton width={400} />
+                  <Skeleton width={300} />
+                </Fragment>
+              ) : (
+                <p className="max-w-xs overflow-hidden text-xl text-gray-400">
+                  {data.payload.description}
+                </p>
+              )}
+            </div>
+          </SkeletonTheme>
         </div>
+        {(isLoading || data.payload.servers.length) && (
+          <div className="flex flex-col items-start justify-center w-full space-y-6 p-10 bg-dark-70 bg-opacity-50 rounded-md">
+            {isLoading ? (
+              <SkeletonTheme color="#222822" highlightColor="#2C322C">
+                <Skeleton width={300} height={30} />
+              </SkeletonTheme>
+            ) : (
+              <h1 className="font-bold text-4xl text-gray-300">
+                <i className="twa twa-1f389" /> {data.payload.name}'s servers
+              </h1>
+            )}
+            <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-2 w-full gap-10 2xl:gap-5 3xl:gap-10 place-content-center">
+              {isLoading ? (
+                <Fragment>
+                  {[...Array(6)].map((el, index) => (
+                    <ServerCardSkeleton key={index} />
+                  ))}
+                </Fragment>
+              ) : (
+                <Fragment>
+                  {data.payload.servers.map((server, index) => (
+                    <ServerCard key={index} {...server} />
+                  ))}
+                </Fragment>
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </StandardLayout>
   );
