a8f1940386b9d8696b1fb1c9ccb10150983c8a9b
Kevin Thomas <kevin.jt2007@gmail.com>
2021-06-19 17:21:27 -0400

    Removed logging

--- src/api/user/edit.js
+++ src/api/user/edit.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import cookie from "js-cookie";
 
 async function editUser(id, name, description) {
-  console.log(cookie.get("token"));
   const { data } = await axios.put(
     process.env.NEXT_PUBLIC_API_URL + `/user/${id}/edit`,
     {
