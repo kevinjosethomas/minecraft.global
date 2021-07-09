84970ac694fe683d2f4fbf0b9a68c382ab2f57c6
Kevin Thomas <kevin.jt2007@gmail.com>
2021-07-09 11:12:54 -0700

    Added Navbar

--- src/ui/components/Navbar/Navbar.tsx
+++ src/ui/components/Navbar/Navbar.tsx
@@ -1,5 +1,37 @@
+import NavElement from "./components/NavElement";
+
 const Navbar = (): JSX.Element => {
-  return <div className="flex flex-row items-center justify-between"></div>;
+  const NavElements = [
+    {
+      id: 1,
+      name: "Discover",
+      icon: "far fa-sparkles",
+      href: "/",
+    },
+    {
+      id: 2,
+      name: "Search",
+      icon: "far fa-search",
+      href: "/search",
+    },
+    {
+      id: 3,
+      name: "Premium",
+      icon: "far fa-gem",
+      href: "/premium",
+    },
+  ];
+
+  return (
+    <div className="flex flex-row items-center justify-between w-full h-20 px-20 bg-dark-800">
+      <div className="flex flex-row items-center justify-start space-x-8">
+        <img src="/images/logo.svg" alt="Logo" className="w-8" />
+        {NavElements.map((element) => (
+          <NavElement key={element.id} {...element} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
