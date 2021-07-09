031dee858d188a629f5de84e37f8e7fbb8c078fa
Kevin Thomas <kevin.jt2007@gmail.com>
2021-07-08 22:05:41 -0700

    Removed all existing code

--- src/assets/animations/core.js
+++
@@ -1,7 +0,0 @@
-const DropdownAnimation = {
-  initial: { height: 0 },
-  animate: { height: "auto", transition: { type: "spring", duration: 0.3 } },
-  exit: { height: 0, transition: { duration: 0.3 } },
-};
-
-export { DropdownAnimation };
