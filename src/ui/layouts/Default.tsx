c3b6b2584e7facc6f7cddd1530a0ac4d5d554b0b
Kevin Thomas <kevin.jt2007@gmail.com>
2021-07-13 14:56:19 -0700

    Added custom background prop

--- src/ui/layouts/Default.tsx
+++ src/ui/layouts/Default.tsx
@@ -2,13 +2,16 @@ import Navbar from "ui/components/Navbar/Navbar";
 
 type Default = {
   children?: React.ReactNode;
+  background: string;
 };
 
 const Default = (props: Default): JSX.Element => {
   return (
-    <div className="flex flex-col items-start justify-start w-full h-full overflow-x-hidden">
+    <div className="flex flex-col items-start justify-start w-full overflow-x-hidden">
       <Navbar />
-      <div className="flex flex-col items-start justify-start w-full h-full p-20 bg-dark-700">
+      <div
+        className={`flex flex-col items-start justify-start w-full h-full p-20 ${props.background}`}
+      >
         {props.children}
       </div>
     </div>
