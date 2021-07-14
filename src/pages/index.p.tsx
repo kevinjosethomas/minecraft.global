4e9039102c81d8b98e8c12506324e2d5af2ca5ee
Kevin Thomas <kevin.jt2007@gmail.com>
2021-07-14 13:11:15 -0700

    Populated home page with data

--- src/pages/index.p.tsx
+++ src/pages/index.p.tsx
@@ -1,10 +1,32 @@
+import { useQuery } from "react-query";
+
+import GetHomeResults from "api/home";
 import Default from "ui/layouts/Default";
+import Listing from "./home/components/Listing";
 import Searchbox from "ui/components/Searchbox/Searchbox";
 
-function Home() {
+function Home(): JSX.Element {
+  const { isLoading, error, data } = useQuery(["HomeResults"], GetHomeResults);
+
   return (
     <Default background="bg-dark-700">
-      <Searchbox />
+      <div className="flex flex-col items-center justify-center w-full space-y-20">
+        <Searchbox />
+        <div className="flex flex-col items-center justify-center w-full space-y-12">
+          <Listing
+            title="Popular Servers"
+            subtitle="The most active servers right now!"
+            icon="far fa-stars"
+            data={isLoading ? null : (data as any[])[0].popular.entries}
+          />
+          <Listing
+            title="Growing Servers"
+            subtitle="New and growing Minecraft servers!"
+            icon="far fa-seedling"
+            data={isLoading ? null : (data as any[])[0].small.entries}
+          />
+        </div>
+      </div>
     </Default>
   );
 }
