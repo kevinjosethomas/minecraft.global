878846d0d967d079aa9049c06b53b74b5e8239de
Kevin Thomas <kevin.jt2007@gmail.com>
2021-07-14 13:10:27 -0700

    Added custom Server type

---
+++ src/lib/types.ts
@@ -0,0 +1,31 @@
+type Server = {
+  server_id: number;
+  host: string;
+  port: number;
+  name: string;
+  vanity: string;
+  owner_id: number;
+  monthly_votes: number;
+  description: string;
+  long_description: string;
+  website_url: string;
+  discord_url: string;
+  trailer_url: string;
+  tags: string[];
+  is_bedrock: boolean;
+  whitelisted: boolean;
+  approved: boolean;
+  premium: boolean;
+  scraped: boolean;
+  archived: boolean;
+  last_updated: Date;
+  created_at: Date;
+  offline_since: Date;
+  online: boolean;
+  favicon: string;
+  players_online: number;
+  player_data: object[];
+  pinged_at: Date;
+};
+
+export type { Server };
