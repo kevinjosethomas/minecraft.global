7903f6dd3c8cadd697bb2d7345aa36a10e3cdd8c
Kevin Thomas <kevin.jt2007@gmail.com>
2021-07-14 13:10:35 -0700

    Added support for react-query

--- src/pages/_app.p.tsx
+++ src/pages/_app.p.tsx
@@ -1,13 +1,25 @@
 import type { AppProps } from "next/app";
 import { AnimateSharedLayout } from "framer-motion";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import "../ui/styles/tailwind.css";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchInterval: false,
+    },
+  },
+});
+
 function App({ Component, pageProps }: AppProps) {
   return (
-    <AnimateSharedLayout>
-      <Component {...pageProps} />
-    </AnimateSharedLayout>
+    <QueryClientProvider client={queryClient}>
+      <AnimateSharedLayout>
+        <Component {...pageProps} />
+      </AnimateSharedLayout>
+    </QueryClientProvider>
   );
 }
 
