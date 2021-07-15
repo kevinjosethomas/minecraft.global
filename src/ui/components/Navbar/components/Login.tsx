d80b0eb27d097ad23335b01a312d4126d6961dac
Kevin Thomas <kevin.jt2007@gmail.com>
2021-07-14 23:12:21 -0700

    Updated login URL in navbar

--- src/ui/components/Navbar/components/Login.tsx
+++ src/ui/components/Navbar/components/Login.tsx
@@ -1,12 +1,12 @@
-import Link from "next/link";
-
 const Login = (): JSX.Element => {
   return (
-    <Link href="/login">
-      <a className="group flex flex-row items-center justify-center w-10 h-10 bg-dark-600 rounded">
-        <i className="far fa-sign-in-alt text-lg text-gray-400 group-hover:text-gray-300 transition duration-500" />
-      </a>
-    </Link>
+    <a
+      href={process.env.NEXT_PUBLIC_DISCORD_LOGIN_URL}
+      rel="nofollow noreferrer"
+      className="group flex flex-row items-center justify-center w-10 h-10 bg-dark-600 rounded"
+    >
+      <i className="far fa-sign-in-alt text-lg text-gray-400 group-hover:text-gray-300 transition duration-500" />
+    </a>
   );
 };
 
