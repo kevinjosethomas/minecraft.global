ecf6a9057c99324d4555fe6c2afcfc5dc4787f76
Kevin Thomas <kevin.jt2007@gmail.com>
2021-07-15 15:14:09 -0700

    Animated user dropdown flow

--- src/ui/components/Navbar/components/User.tsx
+++ src/ui/components/Navbar/components/User.tsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import Link from "next/link";
+import OnOutsideClick from "react-outclick";
+import { motion, AnimatePresence } from "framer-motion";
+import { Fragment, MouseEventHandler, useState } from "react";
 
 type User = {
   user: Record<string, any>;
@@ -18,28 +21,45 @@ const User = (props: User): JSX.Element => {
           } text-lg text-gray-400 group-hover:text-gray-300 transition duration-500`}
         />
       </div>
-      {dropdown && (
-        <div className="absolute flex flex-col items-center justify-center p-4 space-y-2 top-12 right-0 bg-dark-700 border-2 border-gray-800 select-none rounded">
-          <div className="flex flex-col items-start justify-center pr-6">
-            <span className="font-medium text-sm text-gray-400">Logged in as</span>
-            <span className="font-bold text-lg text-gray-300">{props.user.name}</span>
-          </div>
-          <div className="h-0.5 w-full bg-dark-100" />
-          <div className="flex flex-col items-start justify-start w-full">
-            <DropdownElement
-              icon="fas fa-info-circle fa-swap-opacity"
-              title="View Profile"
-              href="/user"
-            />
-            <DropdownElement
-              icon="fas fa-plus-circle fa-swap-opacity"
-              title="Add Server"
-              href="/server/add"
-            />
-            <DropdownElement icon="fas fa-sign-out-alt" title="Log out" href="/server/add" />
-          </div>
-        </div>
-      )}
+      <AnimatePresence>
+        {dropdown && (
+          <OnOutsideClick onOutsideClick={() => setDropdown(false)}>
+            <motion.div
+              className="absolute flex flex-col items-center justify-start p-4 space-y-2 top-12 right-0 bg-dark-700 border-2 border-gray-800 select-none rounded overflow-y-hidden"
+              initial={{ height: 0 }}
+              animate={{ height: "auto" }}
+              exit={{ height: 0, transition: { duration: 0.3 } }}
+            >
+              <div className="flex flex-col items-start justify-center pr-6">
+                <span className="font-medium text-sm text-gray-400">Logged in as</span>
+                <span className="font-bold text-lg text-gray-300">{props.user.name}</span>
+              </div>
+              <div className="h-0.5 w-full bg-dark-100" />
+              <div className="flex flex-col items-start justify-start w-full">
+                <DropdownElement
+                  icon="fas fa-info-circle fa-swap-opacity"
+                  title="View Profile"
+                  href="/user"
+                  redirect
+                />
+                <DropdownElement
+                  icon="fas fa-plus-circle fa-swap-opacity"
+                  title="Add Server"
+                  href="/server/add"
+                  redirect
+                />
+                <DropdownElement
+                  icon="fas fa-sign-out-alt"
+                  title="Log out"
+                  href="/server/add"
+                  onClick={() => {}}
+                  redirect={false}
+                />
+              </div>
+            </motion.div>
+          </OnOutsideClick>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
@@ -47,14 +67,32 @@ const User = (props: User): JSX.Element => {
 type DropdownElement = {
   icon: string;
   title: string;
-  href: string;
+  redirect: boolean;
+  href?: string;
+  onClick?: MouseEventHandler;
 };
 function DropdownElement(props: DropdownElement): JSX.Element {
   return (
-    <div className="flex flex-row items-center justify-start w-full py-2 px-4 pr-12 space-x-2 hover:bg-dark-500 cursor-pointer rounded">
-      <i className={`${props.icon} text-lg text-gray-400`} />
-      <span className="font-medium text-lg text-gray-400 whitespace-nowrap">{props.title}</span>
-    </div>
+    <Fragment>
+      {props.redirect ? (
+        <Link href={props.href as string}>
+          <a className="flex flex-row items-center justify-start w-full py-2 px-4 pr-12 space-x-2 hover:bg-dark-500 cursor-pointer rounded">
+            <i className={`${props.icon} text-lg text-gray-400`} />
+            <span className="font-medium text-lg text-gray-400 whitespace-nowrap">
+              {props.title}
+            </span>
+          </a>
+        </Link>
+      ) : (
+        <div
+          className="flex flex-row items-center justify-start w-full py-2 px-4 pr-12 space-x-2 hover:bg-dark-500 cursor-pointer rounded"
+          onClick={props.onClick}
+        >
+          <i className={`${props.icon} text-lg text-gray-400`} />
+          <span className="font-medium text-lg text-gray-400 whitespace-nowrap">{props.title}</span>
+        </div>
+      )}
+    </Fragment>
   );
 }
 
