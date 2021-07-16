414703484823f8bbff72a8c9d453fea4aec32284
Kevin Thomas <kevin.jt2007@gmail.com>
2021-07-15 20:32:50 -0700

    Made default layout responsive

--- src/ui/layouts/Default.tsx
+++ src/ui/layouts/Default.tsx
@@ -14,7 +14,7 @@ const Default = (props: Default): JSX.Element => {
     <div className="flex flex-col items-start justify-start w-full overflow-x-hidden">
       <Navbar user={props.user} />
       <div
-        className={`flex flex-col items-start justify-start w-full h-full p-20 ${props.background}`}
+        className={`flex flex-col items-start justify-start w-full h-full p-5 md:p-20 ${props.background}`}
       >
         <motion.div
           className="flex flex-col items-start justify-start w-full h-full"
