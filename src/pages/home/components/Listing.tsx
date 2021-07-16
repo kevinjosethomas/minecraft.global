288156a136faadab62e8ddb663e94a7444750766
Kevin Thomas <kevin.jt2007@gmail.com>
2021-07-15 20:33:29 -0700

    Made homepage responsive

--- src/pages/home/components/Listing.tsx
+++ src/pages/home/components/Listing.tsx
@@ -11,14 +11,14 @@ function Listing(props: Listing): JSX.Element {
   return (
     <div className="flex flex-col items-start justify-center w-full space-y-4">
       <div className="flex flex-row items-center justify-start space-x-4">
-        <i className={`${props.icon} text-6xl text-olive-400`} />
+        <i className={`${props.icon} text-5xl md:text-6xl text-olive-400`} />
         <div className="flex flex-col items-start justify-center">
-          <span className="font-bold text-4xl text-gray-300">{props.title}</span>
-          <span className="font-medium text-xl text-gray-400">{props.subtitle}</span>
+          <span className="font-bold text-2xl md:text-4xl text-gray-300">{props.title}</span>
+          <span className="font-medium text-sm md:text-xl text-gray-400">{props.subtitle}</span>
         </div>
       </div>
       {props.data ? (
-        <div className="grid grid-cols-3 w-full">
+        <div className="grid grid-flow-row md:grid-flow-col w-full gap-y-10 md:gap-y-0">
           {props.data.map((server: any) => (
             <ServerCard key={server.server_id} {...server} />
           ))}
