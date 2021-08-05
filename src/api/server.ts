23968170516826f16ce4161cefd97cb1d900e28a
Kevin Thomas <kevin.jt2007@gmail.com>
2021-08-04 21:44:25 -0700

    Added GetTopVoters and UpvoteServer methods

--- src/api/server.ts
+++ src/api/server.ts
@@ -9,4 +9,25 @@ async function GetServer(id: string) {
   }
 }
 
-export { GetServer };
+async function GetTopVoters(id: string) {
+  try {
+    const votes = await axios.get(process.env.NEXT_PUBLIC_API + `/server/${id}/votes/top`);
+    return [votes.data.payload, null];
+  } catch (e) {
+    return [null, e];
+  }
+}
+
+async function UpvoteServer(id: string, playername: string) {
+  try {
+    const upvote = await axios.post(process.env.NEXT_PUBLIC_API + `/server/${id}/vote`, {
+      server_id: id,
+      minecraft_username: playername,
+    });
+    return [upvote.data.payload, null];
+  } catch (e) {
+    return [null, e];
+  }
+}
+
+export { GetServer, GetTopVoters, UpvoteServer };
