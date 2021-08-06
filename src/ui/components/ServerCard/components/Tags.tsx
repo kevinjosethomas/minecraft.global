d66894810164f2c25048b9e68372dc07c028894f
Kevin Thomas <kevin.jt2007@gmail.com>
2021-08-05 17:28:50 -0700

    Added Tag components

---
+++ src/ui/components/ServerCard/components/Tags.tsx
@@ -0,0 +1,29 @@
+import SimplifyNumber from "simplify-number";
+
+import Tag from "./Tag";
+
+type TagsProps = {
+  monthly_votes: number;
+  players_online: number;
+  tags: string[];
+};
+
+function Tags(props: TagsProps): JSX.Element {
+  return (
+    <div className="flex flex-row items-center justify-start space-x-2">
+      <Tag
+        icon="far fa-arrow-alt-up"
+        tag={SimplifyNumber(props.monthly_votes, { decimal: 1 }).toUpperCase()}
+      />
+      <Tag
+        icon="far fa-user"
+        tag={SimplifyNumber(props.players_online, { decimal: 1 }).toUpperCase()}
+      />
+      {props.tags.map((tag: string) => (
+        <Tag key={tag} tag={tag} />
+      ))}
+    </div>
+  );
+}
+
+export default Tags;
