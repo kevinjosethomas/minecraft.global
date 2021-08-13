d8308cd65bf619c08f6f45f1622b725cc78f1ae0
Kevin Thomas <kevin.jt2007@gmail.com>
2021-08-12 18:08:17 -0700

    Added default favicon

--- src/ui/components/ServerCard/components/Identity.tsx
+++ src/ui/components/ServerCard/components/Identity.tsx
@@ -8,7 +8,11 @@ function Identity(props: IdentityProps): JSX.Element {
     <div className="flex flex-row items-center justify-start w-full space-x-4 overflox-x-hidden whitespace-nowrap">
       <div className="relative flex flex-col items-center justify-center overflow-elipsis">
         <div className="absolute w-16 h-16 bg-dark-300 bg-opacity-30 rounded" />
-        <img src={props.favicon} alt={props.name} className="w-16 h-16 min-w-[4rem] rounded" />
+        <img
+          src={props.favicon || "/images/default_server_favicon.png"}
+          alt={props.name}
+          className="w-16 h-16 min-w-[4rem] rounded"
+        />
       </div>
       <span className="font-bold text-5xl text-gray-300 tracking-tight truncate">{props.name}</span>
     </div>
