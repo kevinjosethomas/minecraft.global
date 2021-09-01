9a4e8ac50a74b9615dfa479b4ec2b1f1d6271e6a
Kevin Thomas <kevin.jt2007@gmail.com>
2021-09-01 12:21:15 -0700

    Added .env variable for v1 query parameter

--- src/api/discord.ts
+++ src/api/discord.ts
@@ -2,9 +2,8 @@ import axios from "axios";
 
 async function LoginWithDiscord(code: string) {
   try {
-    // TODO TOMORROW: should send v1 query parameter so backend can change redirect URL
     const { data } = await axios.get(
-      process.env.NEXT_PUBLIC_API + `/login/discord?code=${code}&v1=true`
+      process.env.NEXT_PUBLIC_API + `/login/discord?code=${code}&v1=${process.env.PROD}`
     );
     return [data, null];
   } catch (e) {
