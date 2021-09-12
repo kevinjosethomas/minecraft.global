c296779f1e9fe1f7a7bc847301e8ee980e54dd10
Kevin Thomas <kevin.jt2007@gmail.com>
2021-09-12 10:44:01 -0700

    Addd Navigation buttons

--- src/pages/server/add/index.p.tsx
+++ src/pages/server/add/index.p.tsx
@@ -47,18 +47,52 @@ function AddServer(props: AddServerProps): JSX.Element {
     trailer_url: "",
   });
 
+  const decrementScreen = () => {
+    setActiveScreen(screens.find((screen) => screen.id === activeScreen.id - 1) as any);
+  };
+  const incrementScreen = () => {
+    setActiveScreen(screens.find((screen) => screen.id === activeScreen.id + 1) as any);
+  };
+
   return (
     <Default background="bg-dark-700">
-      <div className="flex flex-row items-start justify-center space-x-10">
+      <div className="flex flex-row items-start justify-center space-x-10 w-full">
         <Navigation
           screens={screens}
           activeScreen={activeScreen}
           setActiveScreen={setActiveScreen}
         />
-        <div className="flex flex-col items-start justify-center space-y-8">
+        <div className="flex flex-col items-start justify-center space-y-8 w-full">
           <div className="flex flex-row items-center justify-between w-full">
             <span className="font-bold text-5xl text-gray-300">{activeScreen.name}</span>
-            <div className="flex flex-row items-center justify-center"></div>
+            <div className="flex flex-row items-center justify-center space-x-4">
+              {activeScreen.id !== 1 && (
+                <div
+                  className="flex flex-row items-center justify-center px-2.5 space-x-2 bg-dark-300 hover:bg-dark-100 rounded select-none cursor-pointer transition duration-300"
+                  onClick={decrementScreen}
+                >
+                  <i className="fas fa-long-arrow-alt-left md:text-lg text-gray-400" />
+                  <span className="font-semibold md:text-lg text-gray-400">Back</span>
+                </div>
+              )}
+              {activeScreen.id === 4 ? (
+                <div
+                  className="flex flex-row items-center justify-center px-2.5 space-x-2 bg-olive-800 hover:bg-olive-700 rounded select-none cursor-pointer transition duration-300"
+                  onClick={incrementScreen}
+                >
+                  <span className="font-semibold md:text-lg text-gray-300">Submit</span>
+                  <i className="fas fa-map-marker-check md:text-lg text-gray-300" />
+                </div>
+              ) : (
+                <div
+                  className="flex flex-row items-center justify-center px-2.5 space-x-2 bg-olive-800 hover:bg-olive-700 rounded select-none cursor-pointer transition duration-300"
+                  onClick={incrementScreen}
+                >
+                  <span className="font-semibold md:text-lg text-gray-300">Next</span>
+                  <i className="fas fa-long-arrow-alt-right md:text-lg text-gray-300" />
+                </div>
+              )}
+            </div>
           </div>
           <activeScreen.screen params={params} setParams={setParams} />
         </div>
