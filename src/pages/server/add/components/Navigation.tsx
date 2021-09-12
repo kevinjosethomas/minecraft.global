9d37d41f247b8bc5f15e11bf14ea0564353e9ef0
Kevin Thomas <kevin.jt2007@gmail.com>
2021-09-11 18:40:31 -0700

    Added Navigation system

---
+++ src/pages/server/add/components/Navigation.tsx
@@ -0,0 +1,43 @@
+type NavigationProps = {
+  screens: Record<string, any>[];
+  activeScreen: Record<string, any>;
+  setActiveScreen: CallableFunction;
+};
+
+function Navigation(props: NavigationProps): JSX.Element {
+  return (
+    <div className="flex flex-col items-center justify-center space-y-3">
+      {props.screens.map((screen) => (
+        <div key={screen.id} className="flex flex-col items-center justify-center space-y-3">
+          <div
+            className={`flex flex-col items-center justify-center w-10 h-10 ${
+              props.activeScreen.id >= screen.id ? "bg-olive-800" : "bg-dark-300"
+            } rounded-full cursor-pointer`}
+            onClick={() =>
+              props.setActiveScreen(props.screens.find((screen2) => screen2.id == screen.id))
+            }
+          >
+            <span
+              className={`font-bold text-xl ${
+                props.activeScreen.id >= screen.id ? "text-gray-300" : "text-gray-400"
+              }`}
+            >
+              {screen.id}
+            </span>
+          </div>
+          {screen.id != props.screens.length &&
+            [1, 2, 3].map((index) => (
+              <div
+                key={index}
+                className={`w-3 h-3 ${
+                  props.activeScreen.id > screen.id ? "bg-olive-800" : "bg-dark-300"
+                } rounded-full`}
+              />
+            ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Navigation;
--- src/pages/server/add/index.p.tsx
+++ src/pages/server/add/index.p.tsx
@@ -5,6 +5,7 @@ import Default from "ui/layouts/Default";
 import Features from "./screens/Features";
 import Votifier from "./screens/Votifier";
 import Description from "./screens/Description";
+import Navigation from "./components/Navigation";
 
 type AddServerProps = {};
 
@@ -42,8 +43,12 @@ function AddServer(props: AddServerProps): JSX.Element {
 
   return (
     <Default background="bg-dark-700">
-      <div className="flex flex-row items-center justify-center">
-        <div className="flex flex-col items-center justify-center"></div>
+      <div className="flex flex-row items-start justify-center space-x-10">
+        <Navigation
+          screens={screens}
+          activeScreen={activeScreen}
+          setActiveScreen={setActiveScreen}
+        />
         <div className="flex flex-col items-start justify-center space-y-8">
           <div className="flex flex-row items-center justify-between w-full">
             <span className="font-bold text-5xl text-gray-300">{activeScreen.name}</span>
