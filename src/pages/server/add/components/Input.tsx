bb9f3400ae22c25b07284226bebdc580a383596f
Kevin Thomas <kevin.jt2007@gmail.com>
2021-09-11 18:42:37 -0700

    Changed border colors

--- src/pages/server/add/components/Input.tsx
+++ src/pages/server/add/components/Input.tsx
@@ -11,7 +11,7 @@ function Input(props: InputProps): JSX.Element {
     <div className="flex flex-col items-start justify-center">
       <span className="font-medium text-2xl text-gray-400">{props.label}</span>
       <input
-        className="w-64 h-8 px-2 py-2 text-gray-400 bg-dark-600 focus:outline-none rounded border-2 border-gray-800"
+        className="w-64 h-8 px-2 py-2 text-gray-400 bg-dark-600 focus:outline-none rounded border-2 border-dark-500"
         value={props.value}
         onChange={props.onChange}
       />
