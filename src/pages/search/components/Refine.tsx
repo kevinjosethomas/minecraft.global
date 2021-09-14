c348d4afc6edb772d4e4b9049ac6f79e13c17251
Kevin Thomas <kevin.jt2007@gmail.com>
2021-09-13 21:57:09 -0700

    Spaced things out more

--- src/pages/search/components/Refine.tsx
+++ src/pages/search/components/Refine.tsx
@@ -12,7 +12,7 @@ function Refine(props: RefineProps): JSX.Element {
   return (
     <div className="sticky top-10 flex flex-col items-start justify-center w-80 p-8 space-y-6 bg-dark-800 rounded">
       <span className="sticky font-bold text-5xl text-gray-300">Refine</span>
-      <div className="flex flex-col items-start justify-center space-y-5">
+      <div className="flex flex-col items-start justify-center space-y-8">
         <Sort parameters={props.parameters} setParameters={props.setParameters} />
         <Filter parameters={props.parameters} setParameters={props.setParameters} />
         <Tags
