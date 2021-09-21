50201f609e031f2893589bc121f5e0a6ae70df5c
Kevin Thomas <kevin.jt2007@gmail.com>
2021-09-20 18:54:08 -0700

    Made Servers page responsive

--- src/pages/user/components/Servers.tsx
+++ src/pages/user/components/Servers.tsx
@@ -7,8 +7,8 @@ type ServersProps = {
 
 function Servers(props: ServersProps): JSX.Element {
   return (
-    <div className="flex flex-row items-center justify-start w-full p-6 md:p-10 bg-dark-800 border-2 border-gray-800 rounded overflow-hidden">
-      <div className="grid grid-cols-3 justify-items-center w-full">
+    <div className="flex flex-row items-center justify-start w-full md:p-10 md:bg-dark-800 md:border-2 md:border-gray-800 md:rounded overflow-hidden">
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-y-10 xl:gap-y-16 2xl:gap-y-0 justify-items-center w-full">
         {!props.isLoading &&
           props.user.servers.map((server: any) => (
             <ServerCard key={server.server_id} {...server} user={props.user} />
--- src/ui/components/ServerCard/components/Identity.tsx
+++ src/ui/components/ServerCard/components/Identity.tsx
@@ -13,10 +13,6 @@ type IdentityProps = {
 function Identity(props: IdentityProps): JSX.Element {
   return (
     <>
-      <ReactTooltip
-        effect="solid"
-        className="!bg-dark-600 !border-2 !border-gray-800 !text-gray-300 !font-medium"
-      />
       <div className="flex flex-row items-center justify-start w-full space-x-4 overflox-x-hidden whitespace-nowrap">
         <div className="relative flex flex-col items-center justify-center overflow-elipsis">
           <div
@@ -24,6 +20,10 @@ function Identity(props: IdentityProps): JSX.Element {
               props.online ? "bg-olive-500" : "bg-red-400"
             } border-4 border-dark-600`}
             data-tip={props.online ? "Online" : "Offline"}
+          />
+          <ReactTooltip
+            effect="solid"
+            className="!bg-dark-600 !border-2 !border-gray-800 !text-gray-300 !font-medium"
           />
           <div className="absolute w-16 h-16 z-30 bg-dark-300 bg-opacity-30 rounded" />
           <img
