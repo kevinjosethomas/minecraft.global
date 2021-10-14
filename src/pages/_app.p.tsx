9b333a0e677f1fd15ef7068662ab8d0f7649de13
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-13 21:36:12 -0700

    Added meta embed tags

--- src/pages/_app.p.tsx
+++ src/pages/_app.p.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import { AnimateSharedLayout } from "framer-motion";
@@ -17,6 +18,38 @@ const queryClient = new QueryClient({
 function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>minecraft.global</title>
+
+        <meta name="title" content="minecraft.global -  Minecraft Server List" />
+        <meta
+          name="description"
+          content="The best website to find new Minecraft servers to play on! Add your Minecraft server here to reach thousands of potential users!"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://minecraft.global/" />
+        <meta property="og:title" content="minecraft.global - Find the best Minecraft servers" />
+        <meta
+          property="og:description"
+          content="The best website to find new Minecraft servers to play on! Add your Minecraft server here to reach thousands of potential users!"
+        />
+        <meta property="og:image" content="/images/embed.png" />
+
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:url" content="https://minecraft.global/" />
+        <meta
+          property="twitter:title"
+          content="minecraft.global - Find the best Minecraft servers"
+        />
+        <meta
+          property="twitter:description"
+          content="The best website to find new Minecraft servers to play on! Add your Minecraft server here to reach thousands of potential users!"
+        />
+        <meta property="twitter:image" content="/images/embed.png" />
+
+        <link rel="shortcut icon" type="image/svg+xml" href="/images/logo.svg" />
+      </Head>
       <AnimateSharedLayout>
         <Toaster position="bottom-right" />
         <Component {...pageProps} />
