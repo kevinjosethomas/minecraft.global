5b1d45585bca55e47dc1fd6d9af32fc6371422dc
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-16 14:27:49 -0700

    Change from login icon to text

--- src/ui/components/Navbar/components/Login.tsx
+++ src/ui/components/Navbar/components/Login.tsx
@@ -3,9 +3,10 @@ const Login = (): JSX.Element => {
     <a
       href={process.env.NEXT_PUBLIC_DISCORD_LOGIN_URL}
       rel="nofollow noreferrer"
-      className="group flex flex-row items-center justify-center w-10 h-10 bg-dark-600 rounded"
+      className="group flex flex-row items-center justify-center px-5 h-10 bg-dark-600 rounded"
     >
-      <i className="far fa-sign-in-alt text-lg text-gray-400 group-hover:text-gray-300 transition duration-500" />
+      <span className="text-lg text-gray-400">login</span>
+      {/* <i className="far fa-sign-in-alt text-lg text-gray-400 group-hover:text-gray-300 transition duration-500" /> */}
     </a>
   );
 };
