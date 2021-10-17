19bd4efcef55e70ac3dc00eab52264298ad0be1d
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-17 12:41:34 -0700

    Added votifier disclaimer

--- src/pages/server/add/screens/Votifier.tsx
+++ src/pages/server/add/screens/Votifier.tsx
@@ -1,4 +1,5 @@
 import Input from "../components/Input";
+import TextArea from "../components/TextArea";
 
 type VotifierProps = {
   params: Record<string, any>;
@@ -23,9 +24,23 @@ function Votifier(props: VotifierProps): JSX.Element {
 
   return (
     <div className="flex flex-col items-start justify-center space-y-6">
+      <div className="flex flex-col items-center justify-start">
+        <span className="font-medium text-xl text-gray-400 max-w-xl">
+          We only support{" "}
+          <a
+            href="https://www.spigotmc.org/resources/nuvotifier.13449/"
+            target="_blank"
+            rel="noreferrer"
+            className="underline format-links"
+          >
+            NuVotifier
+          </a>
+          . This section is optional but we recommend setting up vote rewards!
+        </span>
+      </div>
       <Input label="Votifier Host" value={props.params.votifier_host} onChange={onHostChange} />
       <Input label="Votifier Port" value={props.params.votifier_port} onChange={onPortChange} />
-      <Input
+      <TextArea
         label="Votifier Token / Public Key"
         value={props.params.votifier_token}
         onChange={onTokenChange}
