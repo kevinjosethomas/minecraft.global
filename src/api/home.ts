41afa69c93b74903a9a1ae03df2736473b28f593
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-19 18:11:47 -0700

    Removed timeout

--- src/api/home.ts
+++ src/api/home.ts
@@ -3,24 +3,20 @@ import axios from "axios";
 async function GetHomeResults() {
   // const auctions = axios.get(process.env.NEXT_PUBLIC_API + "/auctions/winners");
   // const growing = axios.get(process.env.NEXT_PUBLIC_API + "/search?amount=4&sort=growth");
-  const sponsored = axios.get(
-    process.env.NEXT_PUBLIC_API + "/server/random/premium"
-  );
+  const sponsored = axios.get(process.env.NEXT_PUBLIC_API + "/server/random/premium");
   const popular = axios.get(process.env.NEXT_PUBLIC_API + "/search?amount=4&sort=upvotes");
   const newly = axios.get(process.env.NEXT_PUBLIC_API + "/search?amount=4&sort=new");
 
   try {
-    setTimeout(async () => {
-      const data = await Promise.all([sponsored, popular, newly]);
-      return [
-        {
-          sponsored: data[0].data.payload,
-          popular: data[1].data.payload,
-          newly: data[2].data.payload,
-        },
-        null,
-      ];
-    }, 100000);
+    const data = await Promise.all([sponsored, popular, newly]);
+    return [
+      {
+        sponsored: data[0].data.payload,
+        popular: data[1].data.payload,
+        newly: data[2].data.payload,
+      },
+      null,
+    ];
   } catch (e) {
     return [null, e];
   }
