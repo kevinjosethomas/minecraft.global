838c531d46f79284b3b8359305b7cb64ab0f1972
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-20 22:37:48 -0700

    More fixes

--- src/pages/server/[id]/index.p.tsx
+++ src/pages/server/[id]/index.p.tsx
@@ -90,7 +90,9 @@ function Server(props: Server): JSX.Element {
           name="keywords"
           content={`${props.server.name}, ${props.server.name} IP Address, ${
             props.server.name
-          } Vote, ${props.server.name} Discord, ${props.server.tags.join(" minecraft servers, ")}`}
+          } Vote, ${props.server.name} Discord, ${props.server.tags.join(
+            " minecraft servers, "
+          )} minecraft servers`}
         />
       </Head>
       {upvoteModal && (
