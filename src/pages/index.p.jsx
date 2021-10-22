775233efdad9a060861dd2cb926011f771871cb2
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-22 10:45:34 -0700

    Minor Changes

--- src/pages/index.p.jsx
+++ src/pages/index.p.jsx
@@ -5,6 +5,10 @@ export default function Home() {
   return (
     <Default>
       <SearchBox />
+      <div className="flex flex-row items-center justify-center w-full space-x-24">
+        <div className="flex flex-col items-start justify-start w-full"></div>
+        <div className="flex flex-col items-start justify-start min-w-[400px]"></div>
+      </div>
     </Default>
   );
 }
