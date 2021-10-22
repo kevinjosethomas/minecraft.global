7d7103ac1fe0a5777cc03d7b5501db01c8e4898b
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-22 12:51:46 -0700

    Added timeout

---
+++ src/ui/components/SearchBox/components/Popup.jsx
@@ -0,0 +1,7 @@
+export default function Popup(props) {
+  return (
+    <div className="absolute top-[85px] left-0 flex flex-col items-start justify-start w-full h-72 rounded-[12px]">
+      <div className="grid grid-cols-3 w-full"></div>
+    </div>
+  );
+}
--- src/ui/components/SearchBox/components/Searchbar.jsx
+++ src/ui/components/SearchBox/components/Searchbar.jsx
@@ -1,18 +1,36 @@
+import { useEffect, useState } from "react";
+
+import Popup from "./Popup";
+
 export default function Searchbar(props) {
+  const [input, setInput] = useState("");
+  const [query, setQuery] = useState("");
+  const [popup, showPopup] = useState(false);
+
+  useEffect(() => {
+    const id = setTimeout(() => setQuery(input), 1000);
+    return () => clearTimeout(id);
+  }, [input]);
+
   return (
-    <div className="flex flex-row items-center justify-start w-full space-x-[16px]">
+    <div className="relative flex flex-row items-center justify-start w-full">
       <input
-        className="flex flex-row items-center justify-start w-full h-[75px] px-6 text-[24px] text-white text-opacity-60 placeholder-white placeholder-opacity-60 bg-white bg-opacity-[0.06] rounded-[12px] focus:outline-none"
+        value={input}
+        className="input-focus-outline flex flex-row items-center justify-start w-full h-[75px] px-6 mr-[8px] text-[24px] text-white text-opacity-60 placeholder-white placeholder-opacity-60 bg-white bg-opacity-[0.06] rounded-[12px]"
         placeholder="Search for Minecraft servers..."
+        onChange={(e) => setInput(e.target.value)}
+        onFocus={() => showPopup(true)}
+        onBlur={() => showPopup(false)}
       />
       <SearchButton />
+      {popup && <Popup />}
     </div>
   );
 }
 
 function SearchButton() {
   return (
-    <div className="flex flex-row items-center justify-center min-w-[75px] min-h-[75px] bg-olive-700 hover:bg-olive-800 rounded-[12px] transition duration-300 cursor-pointer">
+    <div className="flex flex-row items-center justify-center min-w-[75px] min-h-[75px] ml-[8px] bg-olive-700 hover:bg-olive-800 rounded-[12px] transition duration-300 cursor-pointer">
       <i className="far fa-search text-[24px] text-white text-opacity-90" />
     </div>
   );
