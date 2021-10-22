7c2726e8c2e19be6bbf34b11e6862cdf78fe3716
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-22 13:36:13 -0700

    Added ServerCard

--- src/ui/components/SearchBox/components/Popup.jsx
+++ src/ui/components/SearchBox/components/Popup.jsx
@@ -2,11 +2,19 @@ import ServerCard from "./ServerCard";
 
 export default function Popup(props) {
   return (
-    <div className="absolute top-[85px] left-0 flex flex-col items-start justify-start w-full h-72 p-6 rounded-[12px] bg-olive-940 bg-opacity-50 border-2 border-olive-940">
-      <div className="grid grid-cols-3 gap-5 w-full">
-        {props.results.map((server) => (
-          <ServerCard key={server.server_id} {...server} />
-        ))}
+    <div className="absolute top-[85px] left-0 flex flex-col items-start justify-start w-full p-6 rounded-[12px] bg-olive-940 bg-opacity-50 border-2 border-olive-940">
+      <div className="flex flex-col items-start justify-start w-full space-y-2">
+        <div className="flex flex-row items-center justify-start space-x-1.5">
+          <i className="far fa-search text-[20px] text-white text-opacity-80" />
+          <span className="font-medium text-[20px] text-white text-opacity-80 tracking-tight">
+            SEARCH RESULTS...
+          </span>
+        </div>
+        <div className="grid grid-cols-3 gap-5 w-full">
+          {props.results.map((server) => (
+            <ServerCard key={server.server_id} {...server} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/ui/components/SearchBox/components/ServerCard.jsx
+++ src/ui/components/SearchBox/components/ServerCard.jsx
@@ -1,7 +1,11 @@
 export default function ServerCard(props) {
   return (
     <div className="flex flex-row items-start justify-start p-3 space-x-2 hover:bg-white hover:bg-opacity-[0.04] rounded-[6px] cursor-pointer select-none">
-      <img src={props.favicon} alt={`${props.name}'s Logo`} className="rounded-full" />
+      <img
+        src={props.favicon || "/images/default_server_favicon.png"}
+        alt={`${props.name}'s Logo`}
+        className="rounded-full w-[64px] h-[64px]"
+      />
       <div className="flex flex-col items-start justify-start">
         <span className="text-[24px] text-white text-opacity-80">{props.name}</span>
         <div className="flex flex-row items-center justify-start space-x-3">
