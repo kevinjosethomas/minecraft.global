5c02584ea443d1efd87a43d22339b799663efbdd
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-23 09:57:54 -0700

    Minor Changes

--- src/pages/index.p.jsx
+++ src/pages/index.p.jsx
@@ -43,7 +43,7 @@ export async function getServerSideProps(ctx) {
     console.log(results[1]);
     return {
       props: {
-        error: results[1].response.status,
+        error: results[1].response.status || 500,
       },
     };
   }
