b692ebb229602c89f8a27c96e3ef553b1be9cd5d
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-22 23:15:31 -0700

    Made Navbar go to login page

--- src/ui/components/Navbar/components/User.jsx
+++ src/ui/components/Navbar/components/User.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Fragment } from "react";
 
 export default function User(props) {
@@ -10,13 +11,12 @@ export default function User(props) {
 
 function Login() {
   return (
-    <a
-      href={process.env.NEXT_PUBLIC_DISCORD_LOGIN_URL}
-      className="group flex flex-row items-center justify-center px-5 py-1.5 bg-olive-700 hover:bg-olive-800 rounded-[10px] transition duration-300"
-    >
-      <span className="select-none text-[24px] text-white text-opacity-80 group-hover:text-opacity-90 transition duration-300">
-        Login
-      </span>
-    </a>
+    <Link href="/login" passHref>
+      <a className="group flex flex-row items-center justify-center px-5 py-1.5 bg-olive-700 hover:bg-olive-800 rounded-[10px] transition duration-300">
+        <span className="select-none text-[24px] text-white text-opacity-80 group-hover:text-opacity-90 transition duration-300">
+          Login
+        </span>
+      </a>
+    </Link>
   );
 }
