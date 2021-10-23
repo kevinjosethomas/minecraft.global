a1ca8a8403d3b745c753bb57341372f54fc5bcd7
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-22 23:17:34 -0700

    Removed default animation

--- src/ui/layouts/Default.jsx
+++ src/ui/layouts/Default.jsx
@@ -1,5 +1,3 @@
-import { motion } from "framer-motion";
-
 import Navbar from "ui/components/Navbar/Navbar";
 // import Footer from "ui/components/Footer/Footer";
 
@@ -9,14 +7,9 @@ const Default = (props) => {
       <div className="absolute top-0 left-0 bg-gradient w-full h-full" />
       <div className="z-[1] flex flex-col items-center justify-start w-1200 h-full space-y-10">
         <Navbar user={props.user} />
-        <motion.div
-          className="z-[2] flex flex-col items-start justify-start w-full h-full"
-          initial={{ y: 10 }}
-          animate={{ y: 0 }}
-          transition={{ duration: 0.2 }}
-        >
+        <div className="z-[2] flex flex-col items-start justify-start w-full h-full">
           {props.children}
-        </motion.div>
+        </div>
         {/* <Footer /> */}
       </div>
     </div>
