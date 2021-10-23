06ef5244a68ad7b26c50575fbf0c90f3b4080afb
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-23 11:41:47 -0700

    Added padding below

--- src/ui/layouts/Default.jsx
+++ src/ui/layouts/Default.jsx
@@ -6,7 +6,7 @@ const Default = (props) => {
   return (
     <div className="flex flex-col items-center justify-start w-full overflow-x-hidden">
       <div className="absolute top-0 left-0 bg-gradient w-full h-full" />
-      <div className="z-[1] flex flex-col items-center justify-start w-1200 h-full space-y-10">
+      <div className="z-[1] flex flex-col items-center justify-start w-1200 h-full space-y-10 pb-20">
         <Navbar user={props.user} />
         {props.search && <SearchBox defaultResults={props.defaultResults} header={props.header} />}
         <div className="z-[2] flex flex-col items-start justify-start w-full h-full">
