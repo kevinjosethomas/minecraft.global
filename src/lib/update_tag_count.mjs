3ea63b060b8a5eb69a6a7f5d7709373e88a896f3
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-23 16:50:03 -0700

    Minor Changes and google lib

--- src/lib/update_tag_count.mjs
+++ src/lib/update_tag_count.mjs
@@ -9,14 +9,16 @@ import categories from "./tags.mjs";
     const { data } = await axios.get("https://v2api.minecraft.global/tags");
     const servercount = data.payload;
 
+    const sortedTags = [];
+
     tags = [...categories];
     tags.forEach((category, index) => {
       category.tags.forEach((tag, index2) => {
-        tags[index].tags[index2].count = servercount[tag.id];
+        sortedTags.push({ id: tag.id, name: tag.name, count: servercount[tag.id] });
       });
     });
 
-    fs.writeFileSync("./tags.json", JSON.stringify(tags));
+    fs.writeFileSync("./tags.json", JSON.stringify(sortedTags.sort((a, b) => b.count - a.count)));
 
     console.log("UPDATED TAGS SERVER COUNT");
   } catch (e) {
