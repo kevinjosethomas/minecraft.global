2381c2e0286279b620f0a5f646454aa5ec936533
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-22 22:42:32 -0700

    Completed premium page

--- src/pages/premium/index.p.jsx
+++ src/pages/premium/index.p.jsx
@@ -1,65 +1,42 @@
+import Card from "./components/Card";
 import Default from "ui/layouts/Default";
 
-import Tilt from "react-parallax-tilt";
-
 export default function Premium(props) {
-  const features = [
-    { icon: "fad fa-diamond text-olive-500", label: "Premium Badge" },
-    { icon: "far fa-link text-yellow-700", label: "Custom Server URL" },
-    { icon: "fab fa-discord text-[#5865F2]", label: "Exclusive Discord Role" },
-    { icon: "far fa-stars text-red-700", label: "Prominent Server Card" },
-    { icon: "fal fa-chart-bar text-teal-500", label: "Advanced Server Analytics" },
-    {
-      icon: "fal fa-sack-dollar text-orange-500",
-      label: "$5 Monthly Auction Credit",
-    },
-    { icon: "fal fa-heartbeat text-red-700", label: "Support Us" },
-  ];
-
   return (
     <Default>
       <div className="flex flex-row items-center justify-between w-full py-5">
-        <div className="flex flex-col items-start justify-start">
+        <div className="flex flex-col items-start justify-start w-[700px] space-y-4">
           <h1 className="font-bold text-[48px] text-white text-opacity-90 max-w-xl leading-tight">
             Everything you need to grow your server. For only{" "}
             <span className="text-olive-400">$4.99</span> a month!
           </h1>
-        </div>
-        <Tilt tiltMaxAngleX={2} tiltMaxAngleY={2} tiltReverse>
-          <div className="flex flex-col items-start justify-start w-[450px] rounded-[12px] border-2 border-olive-940 overflow-hidden">
-            <div className="flex flex-col items-start justify-center w-full h-48 px-5 bg-gradient-to-br from-olive-800 to-olive-930">
-              <div className="flex flex-row items-center justify-center py-1 px-4 space-x-1.5 bg-white rounded-full">
-                <i className="fad fa-diamond text-[16px] text-olive-500" />
-                <span className="font-medium text-[16px] text-olive-500 select-none">MONTHLY</span>
+          <div className="flex flex-col items-start justify-center space-y-4">
+            <div className="flex flex-col items-start justify-center">
+              <div className="flex flex-row items-center justify-start space-x-2">
+                <i className="fal fa-chart-bar w-[32px] text-teal-500 text-[32px] text-center" />
+                <span className="text-[32px] text-teal-500">Server Analytics</span>
               </div>
-              <span className="font-bold text-[80px] text-white tracking-tight leading-tight select-none">
-                $4.99
-              </span>
+              <p className="text-[24px] text-white text-opacity-70">
+                Premium users get a plugin that regularly transmits data from your Minecraft server
+                to our website! This allows you to track various statistics like player count,
+                upvote count, memory usage and CPU usage for up to 30 days!
+              </p>
             </div>
-            <div className="flex flex-col items-start justify-start w-full p-6 space-y-5 bg-olive-980">
-              <div className="flex flex-col items-start justify-start space-y-1 select-none">
-                <span className="font-bold text-[32px] text-white text-opacity-90">Features</span>
-                {features.map((feature, index) => (
-                  <Feature key={index} {...feature} />
-                ))}
+            <div className="flex flex-col items-start justify-center">
+              <div className="flex flex-row items-center justify-start space-x-2">
+                <i className="fal fa-sack-dollar w-[32px] text-orange-700 text-[32px] text-center" />
+                <span className="text-[32px] text-orange-700">Monthly Auction Credit</span>
               </div>
-              <div className="flex flex-row items-center justify-center w-full py-3 space-x-2 bg-white bg-opacity-[0.08] hover:bg-opacity-10 rounded cursor-pointer transition duration-300">
-                <i className="fad fa-shopping-cart text-[20px] text-olive-400" />
-                <span className="font-bold text-[20px] text-olive-400">SUBSCRIBE</span>
-              </div>
+              <p className="text-[24px] text-white text-opacity-70">
+                All Premium users get $5 worth of Auctions Credit every month! This credit can be
+                used to bid for advertisements in any tag! Credit will only be used if you actually
+                win the auction. Auctions Credit also carries over to the following month!
+              </p>
             </div>
           </div>
-        </Tilt>
+        </div>
+        <Card />
       </div>
     </Default>
-  );
-}
-
-function Feature(props) {
-  return (
-    <div className="flex flex-row items-center justify-start space-x-2">
-      <i className={`${props.icon} w-[30px] text-[24px] text-center`} />
-      <span className="text-[24px] text-white text-opacity-70">{props.label}</span>
-    </div>
   );
 }
