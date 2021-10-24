c557efb3af417925d9fa19e388e7a143341778eb
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-24 16:35:34 -0700

    Minor Changes

--- src/pages/user/[id]/edit/screens/Profile.jsx
+++ src/pages/user/[id]/edit/screens/Profile.jsx
@@ -14,19 +14,21 @@ export default function Profile(props) {
   };
 
   const onDescriptionChange = (e) => {
-    setParameters((parameters) => ({ ...parameters, description: e.target.value.slice(0, 256) }));
+    setParameters((parameters) => ({ ...parameters, description: e.target.value.slice(0, 200) }));
   };
 
   return (
     <div className="flex flex-col items-start justify-start w-full p-10 bg-olive-950 rounded border-2 border-olive-940">
       <h1 className="font-medium text-[50px] text-white text-opacity-90">Edit Profile</h1>
-      <div className="flex flex-col items-start justify-start space-y-4">
-        <Input label="Username" value={parameters.name} onChange={onNameChange} />
-        <TextArea
-          label="Description"
-          value={parameters.description}
-          onChange={onDescriptionChange}
-        />
+      <div className="flex flex-row items-center justify-center">
+        <div className="flex flex-col items-start justify-start space-y-4">
+          <Input label="Username" value={parameters.name} onChange={onNameChange} />
+          <TextArea
+            label="Description"
+            value={parameters.description}
+            onChange={onDescriptionChange}
+          />
+        </div>
       </div>
     </div>
   );
