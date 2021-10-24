f1a3cc9a0aef84aa1b071ba6133e8970cd8f95ba
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-23 18:04:58 -0700

    Bug Fixes

--- src/ui/components/ServerCard/ServerCard.jsx
+++ src/ui/components/ServerCard/ServerCard.jsx
@@ -9,27 +9,25 @@ import Description from "./components/Description";
 
 export default function ServerCard(props) {
   return (
-    <Link href={`/server/${props.server_id}`} passHref>
-      <Container index={props.index} animate={props.animate}>
-        <div className="flex flex-row items-start justify-start space-x-3 w-full">
-          <Favicon favicon={props.favicon} name={props.name} />
-          <Identity
-            name={props.name}
-            host={props.host}
-            port={props.port}
-            monthly_votes={props.monthly_votes}
-            players_online={props.players_online}
-          />
-        </div>
-        <Description description={props.description} />
-        <Tags tags={props.tags} />
-      </Container>
-    </Link>
+    <Container index={props.index} animate={props.animate} server_id={props.server_id}>
+      <div className="flex flex-row items-start justify-start space-x-3 w-full">
+        <Favicon favicon={props.favicon} name={props.name} />
+        <Identity
+          name={props.name}
+          host={props.host}
+          port={props.port}
+          monthly_votes={props.monthly_votes}
+          players_online={props.players_online}
+        />
+      </div>
+      <Description description={props.description} />
+      <Tags tags={props.tags} />
+    </Container>
   );
 }
 
 const Container = (props) => (
-  <Fragment>
+  <Link href={`/server/${props.server_id}`} passHref>
     {props.animate ? (
       <motion.div
         className="flex flex-col items-start justify-start w-full p-6 space-y-2 bg-white bg-opacity-[0.06] hover:bg-opacity-[0.08] transition duration-300 cursor-pointer"
@@ -44,5 +42,5 @@ const Container = (props) => (
         {props.children}
       </div>
     )}
-  </Fragment>
+  </Link>
 );
