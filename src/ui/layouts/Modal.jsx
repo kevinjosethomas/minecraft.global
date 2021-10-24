e0d93d7e26493fdeebfe9f6a1e3b40e851aace97
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-23 22:48:58 -0700

    Added Modal layout

---
+++ src/ui/layouts/Modal.jsx
@@ -0,0 +1,10 @@
+export default function Modal(props) {
+  return (
+    <div
+      className="z-50 absolute top-0 left-0 flex flex-row items-center justify-center w-screen h-screen bg-black bg-opacity-80"
+      onClick={() => props.showModal(false)}
+    >
+      {props.children}
+    </div>
+  );
+}
