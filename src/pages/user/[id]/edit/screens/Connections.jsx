36fe123bac6b95b4e006a39a3009277d26151b4a
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-24 22:27:47 -0700

    Changed code

--- src/pages/user/[id]/edit/screens/Connections.jsx
+++ src/pages/user/[id]/edit/screens/Connections.jsx
@@ -1,26 +1,4 @@
-import { useEffect, useState } from "react";
-
-import { GetUsernameFromUUID } from "api/mojang";
-
 export default function Connections(props) {
-  const [username, setUsername] = useState("...");
-
-  useEffect(() => {
-    (async () => {
-      if (props.minecraft_uuid) {
-        const [response, error] = await GetUsernameFromUUID(props.minecraft_uuid);
-
-        if (error) {
-          return;
-        }
-
-        setUsername(response.name);
-      } else {
-        setUsername("Link your Minecraft Account");
-      }
-    })();
-  }, []);
-
   return (
     <div className="flex flex-col items-start justify-start w-full p-10 space-y-2 bg-olive-950 rounded border-2 border-olive-940">
       <h1 className="font-medium text-[50px] text-white text-opacity-90">Connections</h1>
@@ -28,7 +6,7 @@ export default function Connections(props) {
         {props.discord_id && (
           <div className="flex flex-row items-center justify-start w-[400px] px-4 py-1.5 space-x-2 bg-[#5865F2] hover:bg-opacity-70 rounded-lg transition duration-300 cursor-default">
             <i className="fab fa-discord w-[30px] text-[24px] text-white" />
-            <span className="text-[24px] text-white">{props.discord_id}</span>
+            <span className="text-[24px] text-white">{props.discord_username}</span>
           </div>
         )}
         {props.google_id && (
@@ -40,7 +18,7 @@ export default function Connections(props) {
         {props.minecraft_uuid && (
           <div className="flex flex-row items-center justify-start w-[400px] px-4 py-1.5 space-x-2 bg-olive-800 hover:bg-opacity-80 rounded-lg transition duration-300 cursor-default">
             <i className="far fa-cube w-[30px] text-[24px] text-white" />
-            <span className="text-[24px] text-white">{username}</span>
+            <span className="text-[24px] text-white">{props.minecraft_username}</span>
           </div>
         )}
       </div>
