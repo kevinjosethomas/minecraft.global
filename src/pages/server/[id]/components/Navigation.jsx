bbec2a2bf84a425bd815343a49db70f1ffa36fa8
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-29 18:23:18 -0700

    Added options dropdown

--- src/pages/server/[id]/components/Navigation.jsx
+++ src/pages/server/[id]/components/Navigation.jsx
@@ -1,21 +1,26 @@
+import Options from "./Options";
+
 import { motion, AnimateSharedLayout } from "framer-motion";
 
 export default function Navigation(props) {
   return (
     <div className="flex flex-col items-start justify-start w-full">
-      <div className="flex flex-row items-center justify-start space-x-10">
-        <AnimateSharedLayout>
-          <Screen
-            label="Overview"
-            active={props.screen === "overview"}
-            onClick={() => props.setScreen("overview")}
-          />
-          <Screen
-            label="Comments"
-            active={props.screen === "comments"}
-            onClick={() => props.setScreen("comments")}
-          />
-        </AnimateSharedLayout>
+      <div className="flex flex-row items-center justify-between w-full">
+        <div className="flex flex-row items-center justify-start space-x-10">
+          <AnimateSharedLayout>
+            <Screen
+              label="Overview"
+              active={props.screen === "overview"}
+              onClick={() => props.setScreen("overview")}
+            />
+            <Screen
+              label="Comments"
+              active={props.screen === "comments"}
+              onClick={() => props.setScreen("comments")}
+            />
+          </AnimateSharedLayout>
+        </div>
+        <Options />
       </div>
       <div className="w-full h-[3px] bg-white bg-opacity-10" />
     </div>
@@ -28,7 +33,15 @@ function Screen(props) {
       className="relative flex flex-row items-center justify-start cursor-pointer"
       onClick={props.onClick}
     >
-      <span className="px-2 text-[32px] text-white">{props.label}</span>
+      <span
+        className={`px-2 text-[32px] text-white ${
+          props.active
+            ? "text-opacity-80"
+            : "text-opacity-60 hover:text-opacity-80 transition duration-300"
+        }`}
+      >
+        {props.label}
+      </span>
       {props.active && (
         <motion.div
           layoutId="underline"
---
+++ src/pages/server/[id]/components/Options.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import OnOutsideClick from "react-outclick";
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function Options(props) {
+  const [dropdown, showDropdown] = useState(false);
+
+  return (
+    <div className="relative flex flex-col items-start justify-center">
+      <div
+        className="flex flex-col items-center justify-center w-[40px] h-[40px] hover:bg-white hover:bg-opacity-10 transition duration-300 rounded cursor-pointer"
+        onClick={() => showDropdown((dd) => !dd)}
+      >
+        <i className="far fa-ellipsis-h text-[32px] text-white text-opacity-80" />
+      </div>
+      <AnimatePresence>{dropdown && <Dropdown showDropdown={showDropdown} />}</AnimatePresence>
+    </div>
+  );
+}
+
+function Dropdown(props) {
+  return (
+    <OnOutsideClick onOutsideClick={() => props.showDropdown(false)}>
+      <motion.div
+        className="absolute top-[42px] right-0 flex flex-col items-center justify-start bg-olive-950 rounded border-2 border-olive-930"
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 10, opacity: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <DropdownItem label="Report Server" onClick={() => {}} />
+      </motion.div>
+    </OnOutsideClick>
+  );
+}
+
+function DropdownItem(props) {
+  return (
+    <div className="flex flex-row items-center justify-start pl-2 pr-10 py-1.5 hover:bg-white hover:bg-opacity-[0.03] cursor-pointer transition duration-300">
+      <span className="text-[20px] text-white text-opacity-80 whitespace-nowrap select-none">
+        {props.label}
+      </span>
+    </div>
+  );
+}
