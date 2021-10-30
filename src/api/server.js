6863ecf94bec2c4ffd6688fb527db41c11d7cd6c
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-29 17:06:37 -0700

    Added GetServerByID method

---
+++ src/api/server.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const GetServerByID = async (id) => {
+  try {
+    const response = await axios.get(`${process.env.API_URL}/server/${id}`);
+
+    return [response.payload, null];
+  } catch (e) {
+    return [null, e];
+  }
+};
+
+export { GetServerByID };
