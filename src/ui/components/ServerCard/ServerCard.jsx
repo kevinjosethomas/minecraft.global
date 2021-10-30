9fd9f2781c51b6e4098c0be06ae8a3c7a9734622
Kevin Thomas <kevin.jt2007@gmail.com>
2021-10-30 10:58:16 -0700

    Changed background of premium cards

--- src/ui/components/ServerCard/ServerCard.jsx
+++ src/ui/components/ServerCard/ServerCard.jsx
@@ -8,7 +8,12 @@ import Description from "./components/Description";
 
 export default function ServerCard(props) {
   return (
-    <Container index={props.index} animate={props.animate} server_id={props.server_id}>
+    <Container
+      index={props.index}
+      animate={props.animate}
+      server_id={props.server_id}
+      premium={props.premium}
+    >
       <div className="flex flex-row items-start justify-start space-x-3 w-full">
         <Favicon favicon={props.favicon} name={props.name} />
         <Identity
@@ -26,21 +31,29 @@ export default function ServerCard(props) {
   );
 }
 
-const Container = (props) => (
-  <Link href={`/server/${props.server_id}`} passHref>
-    {props.animate ? (
-      <motion.div
-        className="flex flex-col items-start justify-start w-full p-6 space-y-2 bg-white bg-opacity-[0.06] hover:bg-opacity-[0.08] transition duration-300 cursor-pointer"
-        initial={{ x: -10, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.2, delay: props.index * 0.1 }}
-      >
-        {props.children}
-      </motion.div>
-    ) : (
-      <div className="flex flex-col items-start justify-start w-full p-6 space-y-2 bg-white bg-opacity-[0.06] hover:bg-opacity-[0.08] transition duration-300 cursor-pointer">
-        {props.children}
-      </div>
-    )}
-  </Link>
-);
+const Container = (props) => {
+  const background = props.premium
+    ? "bg-olive-500 bg-opacity-[0.12] hover:bg-opacity-[0.17]"
+    : "bg-white bg-opacity-[0.06] hover:bg-opacity-[0.08]";
+
+  return (
+    <Link href={`/server/${props.server_id}`} passHref>
+      {props.animate ? (
+        <motion.div
+          className={`flex flex-col items-start justify-start w-full p-6 space-y-2 ${background} transition duration-300 cursor-pointer`}
+          initial={{ x: -10, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.2, delay: props.index * 0.1 }}
+        >
+          {props.children}
+        </motion.div>
+      ) : (
+        <div
+          className={`flex flex-col items-start justify-start w-full p-6 space-y-2 ${background} transition duration-300 cursor-pointer`}
+        >
+          {props.children}
+        </div>
+      )}
+    </Link>
+  );
+};
