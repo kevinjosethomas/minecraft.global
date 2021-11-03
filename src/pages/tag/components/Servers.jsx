5c655ee62d772f679a2c4f21e23f72e2ca49b46f
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-02 18:54:37 -0700

    Added title & subtitle

--- src/pages/tag/components/Servers.jsx
+++ src/pages/tag/components/Servers.jsx
@@ -8,6 +8,7 @@ import ServerCard from "ui/components/ServerCard/ServerCard";
 
 export default function Servers(props) {
   const [results, setResults] = useState([...props.results.entries]);
+  const [resultCount, setResultCount] = useState(props.results.total_records);
 
   const loadMore = async (page) => {
     const [response, error] = await SearchByTag(props.tag, 12, page * 12, props.sort);
@@ -34,18 +35,28 @@ export default function Servers(props) {
   }, [props.sort]);
 
   return (
-    <InfiniteScroll
-      pageStart={0}
-      loadMore={loadMore}
-      hasMore={props.results.total_records > results.length}
-      loading={<Loading />}
-    >
-      <div className="flex flex-col items-start justify-start w-full space-y-0.5 rounded-[12px] overflow-hidden">
-        {results.map((server, index) => (
-          <ServerCard key={index} index={index} {...server} animate />
-        ))}
+    <div className="flex flex-col items-start justify-start w-full space-y-3">
+      <div className="flex flex-col items-start justify-start">
+        <span className="text-[32px] text-white text-opacity-90">
+          {props.tag} Minecraft Servers
+        </span>
+        <span className="text-[20px] text-white text-opacity-80">
+          Showing about {resultCount} results...
+        </span>
       </div>
-    </InfiniteScroll>
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={loadMore}
+        hasMore={props.results.total_records > results.length}
+        loading={<Loading />}
+      >
+        <div className="flex flex-col items-start justify-start w-full space-y-0.5 rounded-[12px] overflow-hidden">
+          {results.map((server, index) => (
+            <ServerCard key={index} index={index} {...server} animate />
+          ))}
+        </div>
+      </InfiniteScroll>
+    </div>
   );
 }
 
