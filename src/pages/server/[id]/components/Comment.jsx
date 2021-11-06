07e761a7670f34700fbb4459a4dae843e4970089
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-05 20:08:39 -0700

    Added Comment component

---
+++ src/pages/server/[id]/components/Comment.jsx
@@ -0,0 +1,29 @@
+import moment from "moment";
+
+export default function Comment(props) {
+  const avatar = props.minecraft_uuid
+    ? `https://crafatar.com/avatars/${props.minecraft_uuid}?size=128`
+    : "/images/steve.png";
+
+  return (
+    <div className="flex flex-col items-start justify-start w-full p-5 space-y-1 bg-olive-920 bg-opacity-20 rounded">
+      <div className="flex flex-row items-center justify-between w-full">
+        <div className="flex flex-row items-center justify-start space-x-2">
+          <img src={avatar} alt={`${props.name}'s' Name`} className="w-10 h-10 rounded-sm" />
+          <span className="text-[32px] text-white text-opacity-90">{props.name}</span>
+        </div>
+        <div className="flex flex-row items-center justify-start">
+          <span className="text-[20px] text-white text-opacity-80">
+            Posted on{" "}
+            <span className="font-medium text-opacity-90">
+              {moment(props.created_at).format("MMM Do YYYY")}
+            </span>
+          </span>
+        </div>
+      </div>
+      <div className="flex flex-row items-start justify-start w-full">
+        <p className="text-[20px] text-white text-opacity-70">{props.content}</p>
+      </div>
+    </div>
+  );
+}
