7f6afd2a23de3e84f71a2a6d6235339e302f2458
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-18 16:20:58 -0800

    feat: added remove tags option

--- src/pages/server/[id]/manage/components/Tags.jsx
+++ src/pages/server/[id]/manage/components/Tags.jsx
@@ -15,14 +15,14 @@ export default function Tags(props) {
   }, [modal]);
 
   return (
-    <div className="flex flex-row items-center justify-between w-full space-x-8">
+    <div className="flex flex-row items-start justify-between w-full space-x-8">
       <div className="flex flex-col items-start justify-start">
         <p className="text-2xl text-white text-opacity-80">Server Tags</p>
         <p className="text-lg text-white text-opacity-60 leading-tight">
           Select up to 5 tags that apply to your server
         </p>
       </div>
-      <div className="flex flex-col items-start justify-start min-w-[450px]">
+      <div className="flex flex-col items-start justify-start min-w-[450px] max-w-[450px]">
         <AnimatePresence>
           {modal && (
             <TagsModal
@@ -38,6 +38,19 @@ export default function Tags(props) {
           onClick={() => showModal(true)}
         >
           <p className="text-xl text-white select-none">Choose Tags</p>
+        </div>
+        <div className="flex flex-row flex-wrap items-center justify-start mt-2">
+          {props.tags.map((tag, index) => (
+            <div
+              key={index}
+              className="flex flex-row items-center justify-center mr-2 mb-2 px-2 py-0.5 bg-white cursor-pointer bg-opacity-10 hover:bg-opacity-[0.15] rounded transition duration-300"
+              onClick={() =>
+                props.setDetails((d) => ({ ...d, tags: d.tags.filter((t) => t !== tag) }))
+              }
+            >
+              <p className="text-sm text-white select-none whitespace-nowrap">{tag}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
