3f1240a42e4eae0a20df57d5727d800f859142be
Milo Weinberg <iapetus011@gmail.com>
2021-11-30 18:40:19 -0500

    feat: remove Discord from sidebar as it's unnecessarily redundant

--- src/pages/home/components/Sidebar.jsx
+++ src/pages/home/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import TopTags from "./TopTags";
 export default function Sidebar() {
   return (
     <div className="hidden md:flex flex-col items-start justify-start min-w-[400px] max-w-[400px] space-y-8">
-      <Discord />
+      {/* <Discord /> */}
       {/* <Advertising /> */}
       <Premium />
       <TopTags />
@@ -13,19 +13,19 @@ export default function Sidebar() {
   );
 }
 
-function Discord() {
-  return (
-    <a
-      target="_blank"
-      rel="noreferrer nofollow"
-      href="https://discord.minecraft.global/"
-      className="flex flex-row items-center justify-start w-full px-4 py-2 space-x-3 hover:bg-white hover:bg-opacity-5 rounded transition duration-300"
-    >
-      <i className="fab fa-discord text-3xl text-olive-600" />
-      <p className="text-3xl text-white text-opacity-80">Join our Discord</p>
-    </a>
-  );
-}
+// function Discord() {
+//   return (
+//     <a
+//       target="_blank"
+//       rel="noreferrer nofollow"
+//       href="https://discord.minecraft.global/"
+//       className="flex flex-row items-center justify-start w-full px-4 py-2 space-x-3 hover:bg-white hover:bg-opacity-5 rounded transition duration-300"
+//     >
+//       <i className="fab fa-discord text-3xl text-olive-600" />
+//       <p className="text-3xl text-white text-opacity-80">Join our Discord</p>
+//     </a>
+//   );
+// }
 
 function Advertising() {
   return (
