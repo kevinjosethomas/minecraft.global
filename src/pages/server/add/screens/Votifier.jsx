52e9823a9eac9fd6a11142525433a2b4199ea6e4
Milo Weinberg <iapetus011@gmail.com>
2021-11-30 18:27:22 -0500

    feat: Votifier host pre-fills with the server's address in add new server

--- src/pages/server/add/screens/Votifier.jsx
+++ src/pages/server/add/screens/Votifier.jsx
@@ -22,7 +22,7 @@ export default function Details(props) {
         <Input
           label="Votifier Host"
           description="Your Votifier address or IP (probably the same as your server's address)"
-          value={props.details.votifier.votifier_host}
+          value={props.details.votifier.votifier_host || props.details.host}
           onChange={(e) => onValueChange("votifier_host", e.target.value, 32)}
         />
         <Input
