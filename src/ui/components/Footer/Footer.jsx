faad030f5bd13ad76f218e1bf0e276c2a314ae0e
Kevin Thomas <kevin.jt2007@gmail.com>
2021-11-29 19:17:19 -0800

    feat: minor changes

--- src/ui/components/Footer/Footer.jsx
+++ src/ui/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import Disclaimer from "./components/Disclaimer";
 
 export default function Footer(props) {
   return (
-    <div className="flex flex-col items-center justify-start w-full pt-24 space-y-8">
+    <div className="flex flex-col items-center justify-start w-full pt-4 md:pt-24 space-y-8">
       <Links />
       <div className="w-full h-0.5 bg-white bg-opacity-10" />
       <Disclaimer />
