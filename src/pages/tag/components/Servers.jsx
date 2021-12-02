1b71dcc5f118d81d0809187149fa21771cc13557
Milo Weinberg <iapetus011@gmail.com>
2021-12-02 11:37:03 -0500

    fix tag page query

--- src/pages/tag/components/Servers.jsx
+++ src/pages/tag/components/Servers.jsx
@@ -39,7 +39,7 @@ export default function Servers(props) {
     (async () => {
       const [response, error] = await GetSearchResults({
         ...props.parameters,
-        tag: encodeURIComponent(props.tag),
+        tags: encodeURIComponent(props.tag),
         amount: 12,
       });
 
