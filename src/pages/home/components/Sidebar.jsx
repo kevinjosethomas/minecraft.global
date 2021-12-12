3ecaa25354ef82d82d09c2cdf9056735e1558e49
Kevin Thomas <kevin.jt2007@gmail.com>
2021-12-12 15:33:49 -0800

    feat: modified sidebar

--- src/pages/home/components/Sidebar.jsx
+++ src/pages/home/components/Sidebar.jsx
@@ -5,27 +5,53 @@ import TopTags from "./TopTags";
 export default function Sidebar() {
   return (
     <div className="hidden md:flex flex-col items-start justify-start min-w-[400px] max-w-[400px] space-y-8">
-      {/* <Discord /> */}
+      <div className="flex flex-col items-start justify-start w-full space-y-2">
+        <Discord />
+        <Random />
+        <AddServer />
+      </div>
       {/* <Advertising /> */}
-      <Premium />
+      {/* <Premium /> */}
       <TopTags />
     </div>
   );
 }
 
-// function Discord() {
-//   return (
-//     <a
-//       target="_blank"
-//       rel="noreferrer nofollow"
-//       href="https://discord.minecraft.global/"
-//       className="flex flex-row items-center justify-start w-full px-4 py-2 space-x-3 hover:bg-white hover:bg-opacity-5 rounded transition duration-300"
-//     >
-//       <i className="fab fa-discord text-3xl text-olive-600" />
-//       <p className="text-3xl text-white text-opacity-80">Join our Discord</p>
-//     </a>
-//   );
-// }
+function Discord() {
+  return (
+    <a
+      target="_blank"
+      rel="noreferrer nofollow"
+      href="https://discord.minecraft.global/"
+      className="flex flex-row items-center justify-start w-full px-4 py-2 space-x-3 hover:bg-white hover:bg-opacity-5 rounded transition duration-300"
+    >
+      <i className="fab fa-discord w-[37.5px] text-center text-3xl text-olive-600" />
+      <p className="text-3xl text-white text-opacity-80">Join our Discord</p>
+    </a>
+  );
+}
+
+function Random() {
+  return (
+    <Link href="/random">
+      <a className="flex flex-row items-center justify-start w-full px-4 py-2 space-x-3 hover:bg-white hover:bg-opacity-5 rounded transition duration-300">
+        <i className="fad fa-random w-[37.5px] text-center text-3xl text-olive-600" />
+        <p className="text-3xl text-white text-opacity-80">Random Server</p>
+      </a>
+    </Link>
+  );
+}
+
+function AddServer() {
+  return (
+    <Link href="/server/add">
+      <a className="flex flex-row items-center justify-start w-full px-4 py-2 space-x-3 hover:bg-white hover:bg-opacity-5 rounded transition duration-300">
+        <i className="fad fa-plus-circle w-[37.5px] text-center text-3xl text-olive-600" />
+        <p className="text-3xl text-white text-opacity-80">Add Your Server</p>
+      </a>
+    </Link>
+  );
+}
 
 function Advertising() {
   return (
