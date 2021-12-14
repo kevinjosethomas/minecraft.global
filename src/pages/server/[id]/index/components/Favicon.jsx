8e55dd6324648e47404d8285085140e9fb09e7cc
Kevin Thomas <kevin.jt2007@gmail.com>
2021-12-13 21:55:58 -0800

    feat: minor bug fixes

--- src/pages/server/[id]/index/components/Favicon.jsx
+++ src/pages/server/[id]/index/components/Favicon.jsx
@@ -1,7 +1,7 @@
 export default function Favicon(props) {
   return (
     <img
-      className="rounded"
+      className="w-[64px] rounded"
       src={props.favicon || "/images/default_server_favicon.png"}
       alt={`${props.name}'s favicon'`}
       draggable="false"
