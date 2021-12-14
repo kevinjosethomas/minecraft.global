18e0a3f3cf3a79ab861f1602a944bf165708ec3e
Kevin Thomas <kevin.jt2007@gmail.com>
2021-12-13 21:41:42 -0800

    feat: removed Discord from sidebar

--- src/pages/home/components/Sidebar.jsx
+++ src/pages/home/components/Sidebar.jsx
@@ -6,7 +6,7 @@ export default function Sidebar() {
   return (
     <div className="hidden md:flex flex-col items-start justify-start min-w-[400px] max-w-[400px] space-y-8">
       <div className="flex flex-col items-start justify-start w-full space-y-2">
-        <Discord />
+        {/* <Discord /> */}
         <Random />
         <AddServer />
       </div>
