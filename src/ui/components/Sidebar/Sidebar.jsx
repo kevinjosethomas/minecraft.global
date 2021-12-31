65e6ddef3f3d9f4bed01683e28e21cb86aaa1570
Kevin Thomas <kevin.jt2007@gmail.com>
2021-12-30 17:54:58 -0800

    feat: temporarily removed premium

--- src/ui/components/Sidebar/Sidebar.jsx
+++ src/ui/components/Sidebar/Sidebar.jsx
@@ -7,7 +7,7 @@ export default function Sidebar(props) {
     <div className="hidden md:flex flex-col items-start justify-start min-w-[400px] max-w-[400px] space-y-8">
       {/* <div className="flex flex-col items-start justify-start w-full space-y-2"></div> */}
       {!props.addServer && <AddServer />}
-      <Premium />
+      {/* <Premium /> */}
       <Tags />
     </div>
   );
