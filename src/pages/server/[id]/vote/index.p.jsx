471f5ce1ace12a7f5976b8eb96ae5403d539e1e6
Kevin Thomas <kevin.jt2007@gmail.com>
2022-01-26 17:28:08 -0800

    feat: added oreomc advertisement

--- src/pages/server/[id]/vote/index.p.jsx
+++ src/pages/server/[id]/vote/index.p.jsx
@@ -113,10 +113,10 @@ export default function UpvoteServer(props) {
             <TopVoters server_id={props.server.server_id} />
           </div>
         </div>
-        <a
+        {/* <a
           href="https://discord.minecraft.global/"
           target="_blank"
-          rel="noreferrer"
+          rel="noopener"
         >
           <img
             src="/images/votepage-ad.png"
@@ -124,7 +124,20 @@ export default function UpvoteServer(props) {
             alt="Rent this advertisement spot"
             className="rounded"
           />
-        </a>
+        </a> */}
+        <div className="flex flex-col items-center justify-start space-y-2 rounded border-2 border-olive-920 bg-olive-950 p-4">
+          <p className="text-2xl font-medium text-white text-opacity-90">
+            Sponsored
+          </p>
+          <a href="https://oreomc.com/" target="_blank" rel="noopener">
+            <img
+              src="/images/oreomc-ad.png"
+              draggable="false"
+              alt="Semi Vanilla MC 1.18.1 Survival Join Now"
+              className="rounded"
+            />
+          </a>
+        </div>
         {/* <Similar tag={props.tag} /> */}
       </div>
     </Default>
