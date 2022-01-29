5c3a251bb50cfeb0aff5459057f1e8759d9f8251
Kevin Thomas <kevin.jt2007@gmail.com>
2022-01-28 18:52:39 -0800

    feat: added all API handlers

--- src/api/advertisements.js
+++ src/api/advertisements.js
@@ -1,23 +1,106 @@
 import axios from "axios";
 
-const FetchWeeeklyAdvertisements = () => {
+const FetchAdvertisementPrices = async () => {
   try {
-    return [
-      {
-        payload: [
-          {
-            week_id: 1,
-            slot_home_page: 10,
-            slot_vote_page: 8,
-            created_at: new Date(),
-          },
-        ],
-      },
-      null,
-    ];
+    const response = await axios.get(`${process.env.API_URL}/a/prices`);
+
+    return [response.data, null];
   } catch (e) {
     return [null, e];
   }
 };
 
-export { FetchWeeeklyAdvertisements };
+const FetchWeeeklyAdvertisements = async () => {
+  try {
+    const response = await axios.get(`${process.env.API_URL}/a/slots`);
+
+    return [response.data, null];
+  } catch (e) {
+    return [null, e];
+  }
+};
+
+const FetchUserProducts = async (user_id, token) => {
+  try {
+    const response = await axios.get(
+      `${process.env.API_URL}/a/user/${user_id}/products`,
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+
+    return [response.data, null];
+  } catch (e) {
+    return [null, e];
+  }
+};
+
+const CreateNewProduct = async (name, url, image, token) => {
+  try {
+    const response = await axios.post(
+      `${process.env.API_URL}/a/product/new`,
+      {
+        name,
+        url,
+      },
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+
+    return [response.data, null];
+  } catch (e) {
+    return [null, e];
+  }
+};
+
+const EditProduct = async (product_id, name, url, image, token) => {
+  try {
+    const response = await axios.put(
+      `${process.env.API_URL}/a/product/${product_id}`,
+      {
+        name,
+        url,
+      },
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+
+    return [response.data, null];
+  } catch (e) {
+    return [null, e];
+  }
+};
+
+const DeleteProduct = async (product_id, token) => {
+  try {
+    const response = await axios.delete(
+      `${process.env.API_URL}/a/product/${product_id}`,
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+
+    return [response.data, null];
+  } catch (e) {
+    return [null, e];
+  }
+};
+
+export {
+  FetchAdvertisementPrices,
+  FetchWeeeklyAdvertisements,
+  FetchUserProducts,
+  CreateNewProduct,
+  EditProduct,
+  DeleteProduct,
+};
