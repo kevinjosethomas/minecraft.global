b2011f79df8e7f5641283084506106b7c75b4eec
Kevin Thomas <kevin.jt2007@gmail.com>
2022-02-20 16:53:30 -0800

    feat: added success page

--- src/pages/advertise/banner/screens/Purchase/modals/Purchase.jsx
+++ src/pages/advertise/banner/screens/Purchase/modals/Purchase.jsx
@@ -1,15 +1,22 @@
 import cookie from "js-cookie";
 import { useState } from "react";
+import toast from "react-hot-toast";
 import { motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 import Modal from "ui/layouts/Modal";
 import Dropdown from "../components/Dropdown";
 import { CreateAdvertisementSession } from "api/advertisements";
 
 export default function Purchase(props) {
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
   const [product, setProduct] = useState(props.products[0]);
 
   const submit = async () => {
+    setLoading(true);
+
     const token = cookie.get("token");
     const [response, error] = await CreateAdvertisementSession(
       product.product_id,
@@ -18,7 +25,32 @@ export default function Purchase(props) {
       token
     );
 
-    console.log(response);
+    if (error) {
+      switch (error.response?.status) {
+        case 401:
+          toast.error("You do not own this product!");
+          break;
+        case 403:
+          if (
+            error.response?.data.payload.detail.error ==
+            "slot_already_purchased"
+          ) {
+            toast.error("This advertisement slot is already purchased!");
+          } else {
+            toast.error("You cannot purchase this advertisement slot anymore!");
+          }
+          break;
+        case 404:
+          toast.error("The product you selected is not available!");
+          break;
+        default:
+          toast.error("An unknown error occured, please try again later!");
+      }
+      return;
+    }
+
+    setLoading(false);
+    router.push(response.payload);
   };
 
   return (
@@ -65,9 +97,18 @@ export default function Purchase(props) {
               className="flex cursor-pointer items-center justify-center rounded-lg bg-olive-800 px-6 py-2 transition duration-300 hover:bg-olive-600"
               onClick={submit}
             >
-              <p className="select-none text-xl text-white text-opacity-90">
-                Purchase
-              </p>
+              {loading ? (
+                <img
+                  draggable="false"
+                  src="/images/loading.svg"
+                  className="h-7 w-7"
+                  alt="Loading"
+                />
+              ) : (
+                <p className="select-none text-xl text-white text-opacity-90">
+                  Purchase
+                </p>
+              )}
             </div>
           </div>
         </div>
---
+++ src/pages/advertise/banner/success/index.p.jsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+
+import Default from "ui/layouts/Default";
+import { GetLoggedInUser } from "api/login";
+
+export default function Success(props) {
+  return (
+    <Default
+      user={props.user}
+      title="Purchase Complete - Minecraft Server List"
+    >
+      <div className="relative flex w-full flex-col">
+        <div className="absolute z-10 flex h-full w-full flex-col items-center justify-center space-y-4">
+          <p className="max-w-2xl select-none text-center text-6xl font-bold text-white">
+            Thanks for purchasing an advertisement!{" "}
+            <img
+              alt="Heart"
+              src="/images/heart.png"
+              className="inline w-10"
+              draggable="false"
+            />
+          </p>
+          <Link href="/advertise/dashboard">
+            <a className="flex select-none items-center justify-center rounded-lg bg-olive-700  px-6 py-3 transition duration-300 hover:bg-olive-800">
+              <p className="text-2xl font-medium text-white">
+                Visit Advertisement Dashboard
+              </p>
+            </a>
+          </Link>
+        </div>
+        <img
+          src="/images/illustrations/mobs.png"
+          alt="Mobs"
+          className="opacity-20"
+        />
+      </div>
+    </Default>
+  );
+}
+
+export async function getServerSideProps(ctx) {
+  try {
+    const [user, error] = await GetLoggedInUser(ctx);
+
+    if (error) {
+      return {
+        redirect: {
+          destination: "/login",
+          permanent: false,
+        },
+      };
+    } else {
+      return {
+        props: {
+          user: user,
+        },
+      };
+    }
+  } catch (e) {
+    console.log(e);
+    return {
+      props: {
+        error: 500,
+      },
+    };
+  }
+}
