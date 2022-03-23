2061caff417b4d11c70c0e41bc0f5409f58f5276
Kevin Thomas <kevin.jt2007@gmail.com>
2022-03-22 19:34:01 -0700

    feat: added notifications to navbar

--- src/ui/components/Navbar/components/Identity.jsx
+++ src/ui/components/Navbar/components/Identity.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import { AnimatePresence, motion, useAnimation } from "framer-motion";
 
@@ -47,26 +48,33 @@ function Info(props) {
     : "/images/steve.png";
 
   return (
-    <div
-      className="flex cursor-pointer items-center justify-center space-x-8 rounded-lg bg-olive-900 px-4 py-3 transition duration-300 hover:bg-olive-910"
-      onClick={() => props.showDropdown((dd) => !dd)}
-    >
-      <div className="flex flex-row items-center justify-start space-x-3">
-        <Avatar avatar={avatar} name={props.name} />
-        <div className="flex flex-col items-start justify-start">
-          <p className="select-none text-lg leading-tight text-white">
-            {props.name}
-          </p>
-          <p className="text-xs leading-tight text-white text-opacity-60">
-            View Menu
-          </p>
+    <div className="flex items-center space-x-6">
+      <Link href="/notifications">
+        <a>
+          <i className="fas fa-bell text-2xl text-white text-opacity-80 transition duration-300 hover:text-opacity-100" />
+        </a>
+      </Link>
+      <div
+        className="flex cursor-pointer items-center justify-center space-x-8 rounded-lg bg-olive-900 px-4 py-3 transition duration-300 hover:bg-olive-910"
+        onClick={() => props.showDropdown((dd) => !dd)}
+      >
+        <div className="flex flex-row items-center justify-start space-x-3">
+          <Avatar avatar={avatar} name={props.name} />
+          <div className="flex flex-col items-start justify-start">
+            <p className="select-none text-lg leading-tight text-white">
+              {props.name}
+            </p>
+            <p className="text-xs leading-tight text-white text-opacity-60">
+              View Menu
+            </p>
+          </div>
         </div>
+        <motion.i
+          animate={props.animation}
+          className="far fa-angle-down text-xl text-white text-opacity-60"
+          transition={{ duration: 0.3 }}
+        />
       </div>
-      <motion.i
-        animate={props.animation}
-        className="far fa-angle-down text-xl text-white text-opacity-60"
-        transition={{ duration: 0.3 }}
-      />
     </div>
   );
 }
