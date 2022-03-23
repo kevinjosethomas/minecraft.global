506cd0a41ce0186e4eeba05e71955dcb247476a8
Kevin Thomas <kevin.jt2007@gmail.com>
2022-03-22 19:13:03 -0700

    feat: mionor changes

--- src/pages/home/components/Servers.jsx
+++ src/pages/home/components/Servers.jsx
@@ -76,7 +76,7 @@ export default function Servers(props) {
       hasMore={canFetchmore}
       loader={<Loading />}
     >
-      <div className="flex w-full flex-col items-start justify-start space-y-8 overflow-hidden rounded-[12px]">
+      <div className="flex w-full flex-col items-start justify-start space-y-8 overflow-hidden rounded-2xl">
         {results
           .filter((result) => result.results.length)
           .map((result, index) => (
