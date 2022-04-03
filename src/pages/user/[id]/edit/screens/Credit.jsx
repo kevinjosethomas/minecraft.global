3e6af39aeb9a4ee4e72dcc811453d8abb7a79b67
Milo Weinberg <iapetus011@gmail.com>
2022-04-03 10:59:12 -0400

    update credit screen

--- src/pages/user/[id]/edit/screens/Credit.jsx
+++ src/pages/user/[id]/edit/screens/Credit.jsx
@@ -8,16 +8,16 @@ export default function Credit(props) {
         <div className="flex min-w-[16rem] select-none flex-col items-start justify-start space-y-1.5 rounded border-2 border-olive-930 bg-olive-940 p-5">
           <p className="text-xl text-white text-opacity-80">You have</p>
           <p className="text-4xl font-medium tracking-wide text-white text-opacity-90">
-            ${Math.round(props.user.auction_credit * 100) / 100}
+            ${Math.round(props.user.credit * 100) / 100}
           </p>
           <p className="text-xl text-white text-opacity-80">
             of Advertising Credit
           </p>
         </div>
         <p className="text-lg text-white text-opacity-80">
-          You can use this Advertising Credit to buy weekly advertisement spots
-          once our Auctions are open! Get $5 of free Advertising Credit every
-          month for every server you subscribe to Premium for :)
+          You can use this Advertising Credit to buy weekly advertisement spots!
+          Get $5 of free Advertising Credit every month for every server you
+          subscribe to Premium for :)
         </p>
       </div>
     </div>
